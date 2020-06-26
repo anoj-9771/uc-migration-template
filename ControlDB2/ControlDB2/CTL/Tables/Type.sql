3332949a7a909813dfbb9f560ae487d628b0e2d3
Stephen Lundall <Stephen.Lundall@insight.com>
2020-06-26 02:09:51 +0000

    Deleted ControlDB2

--- ControlDB2/ControlDB2/CTL/Tables/Type.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [CTL].[Type] (
-    [TypeId]      INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        VARCHAR (250)  NULL,
-    [Description] VARCHAR (1000) NULL,
-    [Format]      VARCHAR (50)   NULL,
-    CONSTRAINT [PK_Type] PRIMARY KEY CLUSTERED ([TypeId] ASC)
-);
-
--- ControlDB2/ControlDB2/CTL/Tables/Watermark.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [CTL].[Watermark] (
-    [WatermarkId] BIGINT        IDENTITY (1, 1) NOT NULL,
-    [DatasetId]   BIGINT        NULL,
-    [Column]      VARCHAR (250) NULL,
-    [Operator]    VARCHAR (5)   NULL,
-    [TypeId]      INT           NULL
-);
-
