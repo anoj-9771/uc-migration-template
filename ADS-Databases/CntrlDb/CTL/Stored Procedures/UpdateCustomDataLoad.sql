9f598d5f0d4390fc4cbf3c040aa9775629652bb6
Staub <Jackson.Staub@insight.com>
2021-06-11 12:06:37 +1000

    a

--- ADS-Databases/CntrlDb/CTL/Stored Procedures/UpdateCustomDataLoad.sql
+++
@@ -1,8 +0,0 @@
-﻿create procedure CTL.UpdateCustomDataLoad (
-@TableName varchar(255),
-@StartDate date)
-AS
-
-UPDATE CTL.CustomDataLoad SET ExtractionComplete = 1
-WHERE TableName = @TableName
-AND StartDate = @StartDate
