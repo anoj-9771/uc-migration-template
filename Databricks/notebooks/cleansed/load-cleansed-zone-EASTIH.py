637e27a7efca37ed51fa8a37331ed8980adf81da
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2021-07-29 07:45:40 +0000

    ref table update

--- Databricks/notebooks/cleansed/load-cleansed-zone-EASTIH.py
+++ Databricks/notebooks/cleansed/load-cleansed-zone-EASTIH.py
@@ -116,7 +116,7 @@ print(data_load_mode)
 #Delta and SQL tables are case Insensitive. Seems Delta table are always lower case
 delta_cleansed_tbl_name = "{0}.{1}".format(ADS_DATABASE_CLEANSED, "stg_"+source_object)
 delta_raw_tbl_name = "{0}.{1}".format(ADS_DATABASE_RAW, source_object)
-delta_raw_tbl_name = "raw.sap_eastih"
+
 
 #Destination
 print(delta_cleansed_tbl_name)
