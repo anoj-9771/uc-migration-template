0ceb3834638edb00d3a2f51c3f9ad6e5762e52f5
o5s5@sydneywater.com.au <o5s5@sydneywater.com.au>
2021-07-30 04:23:18 +0000

    Create Dim Property
    Create Dim Property

---
+++ Databricks/notebooks/curated/Common/functions/common-functions-dimensions.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %run ./dimensions/Property
+
+# COMMAND ----------
+
+#%run ./dimensions/OfferingSkillsPoint
+
+# COMMAND ----------
+
+#%run ./dimensions/UnitOffering
