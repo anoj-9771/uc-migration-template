5ec2c521fcf10d6d4ad5bba42dba5b4f587c98a2
Arman Hosseinzadeh <o3an@sydneywater.com.au>
2021-09-07 13:53:40 +1000

    renamed references to Trusted to Cleansed in stored procedures and views Added new temporary procedures and table to test Technical Reconciliation updates in ADF

---
+++ ADS-Databases/CntrlDb/CTL/Stored Procedures/UpdateManifestDeltaRecords_New.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [CTL].[UpdateManifestDeltaRecords_New] (
+	@BatchExecutionLogID bigint,
+	@TaskExecutionLogID bigint,
+	@SourceFileName varchar(1000),
+	@RecordsDeltaTable bigint)
+AS
+
+BEGIN
+	UPDATE CTL.ControlManifest_New
+	SET RecordCountDeltaTable = @RecordsDeltaTable
+	WHERE BatchExecutionLogID = @BatchExecutionLogID
+		AND TaskExecutionLogID = @TaskExecutionLogID
+		AND SourceFileName = @SourceFileName
+END
