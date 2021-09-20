502bb5840ca846af352aa842914aa3211d6eeaa5
ARTHUR SANNEN <oydm@sydneywater.com.au>
2021-09-20 07:06:04 +0000

    Updated 16 files under /Databricks/notebooks/cleansed/ACCESS REF

--- Databricks/notebooks/cleansed/ACCESS REF/load-cleansed-zone-Z309_TPDEREADMETH.py
+++ Databricks/notebooks/cleansed/ACCESS REF/load-cleansed-zone-Z309_TPDEREADMETH.py
@@ -204,6 +204,8 @@ print(f'Number of rows: {df_updated_column.count()}')
 # DBTITLE 1,12. Save Data frame into Cleansed Delta table (Final)
 #Save Data frame into Cleansed Delta table (final)
 DeltaSaveDataframeDirect(df_updated_column, "t", source_object, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", "")
+#verify and, if necessary, update schema definition
+verifyTableSchema(f'{ADS_DATABASE_CLEANSED}.t_{source_object}',newSchema)
 
 # COMMAND ----------
 
