4bae9b8f2ea67f92f5eceb533ed5c9134f88eb43
Jackson Staub <ozqc@sydneywater.com.au>
2021-11-16 07:49:52 +0000

    Revert "Deleted notebooks"

---
+++ Databricks/notebooks/curated/common/functions/common-functions-facts.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %run ./commonBilledWaterConsumptionIsu
+
+# COMMAND ----------
+
+# MAGIC %run ./commonBilledWaterConsumptionAccess
+
+# COMMAND ----------
+
+# MAGIC %run ./facts/BilledWaterConsumption
+
+# COMMAND ----------
+
+# MAGIC %run ./facts/BilledWaterConsumptionDaily
+
+# COMMAND ----------
+
+#%run ./facts/fact_example_2
---
+++ Databricks/notebooks/curated/common/functions/dimensions/Date.py
@@ -0,0 +1,77 @@
+# Databricks notebook source
+###########################################################################################################################
+# Function: getDate
+#  GETS Date DIMENSION 
+# Returns:
+#  Dataframe of transformed dates
+#############################################################################################################################
+# Method
+# 1.Create Function
+# 2.Load Cleansed layer table data into dataframe and transform
+# 3.JOIN TABLES
+# 4.UNION TABLES
+# 5.SELECT / TRANSFORM
+#############################################################################################################################
+#1.Create Function
+def getDate():
+  
+  #spark.udf.register("TidyCase", GeneralToTidyCase)  
+  
+  #DimProperty
+  #2.Load Cleansed layer table data into dataframe
+  dateDf = spark.sql(f"SELECT  * \
+                                   from {ADS_DATABASE_CLEANSED}.isu_scal_tt_date")
+ 
+  #3.JOIN TABLES  
+  
+  
+  #4.UNION TABLES
+
+  #5.SELECT / TRANSFORM
+  df = dateDf.selectExpr( \
+                          "calendarDate" \
+                          ,"dayOfWeek" \
+                          ,"date_format(calendarDate, 'EEEE') as dayName" \
+                          ,"dayOfMonth" \
+                          ,"dayOfYear" \
+                          ,"monthOfYear" \
+                          ,"date_format(calendarDate, 'MMMM') as monthName" \
+                          ,"quarterOfYear" \
+                          ,"halfOfYear" \
+                          ,"monthStartDate" \
+                          ,"monthEndDate" \
+                          ,"cast(cast(calendarYear as string)||'-01-01' as date) as yearStartDate" \
+                          ,"cast(cast(calendarYear as string)||'-12-31' as date) as yearEndDate" \
+                          ,"cast(case when date_format(calendarDate,'M') > 6 then date_format(calendarDate,'yyyy') + 1 else date_format(calendarDate,'yyyy') end as int) as financialYear" \
+                          ,"cast(cast(cast(case when date_format(calendarDate,'M') > 6 then date_format(calendarDate,'yyyy') else date_format(calendarDate,'yyyy') - 1 end as int) as string)||'-07-01' as date)  as financialYearStartDate" \
+                          ,"cast(cast(cast(case when date_format(calendarDate,'M') > 6 then date_format(calendarDate,'yyyy') + 1 else date_format(calendarDate,'yyyy') end as int) as string)||'-06-30' as date)  as financialYearEndDate" \
+                          ,"cast(case when date_format(calendarDate,'M') > 6 then date_format(calendarDate,'M') - 6 else date_format(calendarDate,'M') + 6 end as int)  as monthOfFinancialYear" \
+                          ,"case when quarter(calendarDate) < 3 then quarter(calendarDate) + 2 else quarter(calendarDate) - 2 end as quarterOfFinacialYear" \
+                          ,"case when quarter(calendarDate) < 3 then 2 else 1 end as halfOfFinacialYear" \
+                       )
+  #6.Apply schema definition
+  newSchema = StructType([
+                            StructField("calendarDate", DateType(), False),
+                            StructField("dayOfWeek", IntegerType(), False),
+                            StructField("dayName", StringType(), False),
+                            StructField("dayOfMonth", IntegerType(), False),
+                            StructField("dayOfYear", IntegerType(), False),
+                            StructField("monthOfYear", IntegerType(), False),
+                            StructField("monthName", StringType(), False),
+                            StructField("quarterOfYear", IntegerType(), False),
+                            StructField("halfOfYear", IntegerType(), False),
+                            StructField("monthStartDate", DateType(), False),
+                            StructField("monthEndDate", DateType(), False),
+                            StructField("yearStartDate", DateType(), False),
+                            StructField("yearEndDate", DateType(), False),
+                            StructField("financialYear", StringType(), False),
+                            StructField("financialYearStartDate", DateType(), False),
+                            StructField("financialYearEndDate", DateType(), True),
+                            StructField("monthOfFinancialYear", IntegerType(), False),
+                            StructField("quarterOfFinancialYear", IntegerType(), False),
+                            StructField("halfOfFinancialYear", IntegerType(), False)
+                      ])
+  
+  df = spark.createDataFrame(df.rdd, schema=newSchema)
+  return df
+
