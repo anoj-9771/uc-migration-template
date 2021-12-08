cf5cf3e4ad9c47e551d9051155418bc009afe973
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2021-12-08 01:46:53 +0000

    Dim Fixes

---
+++ Databricks/notebooks/curated/common/functions/dimensions/BusinessPartnerGroup.py
@@ -0,0 +1,99 @@
+# Databricks notebook source
+###########################################################################################################################
+# Function: getBusinessPartnerGroup
+#  GETS Business Partner Group DIMENSION 
+# Returns:
+#  Dataframe of transformed Metery
+#############################################################################################################################
+# Method
+# 1.Create Function
+# 2.Load Cleansed layer table data into dataframe and transform
+# 3.JOIN TABLES
+# 4.UNION TABLES
+# 5.SELECT / TRANSFORM
+#############################################################################################################################
+#1.Create Function
+
+def getBusinessPartnerGroup():
+    #spark.udf.register("TidyCase", GeneralToTidyCase) 
+
+    #2.Load Cleansed layer table data into dataframe
+    #Business Partner Group Data from SAP ISU
+    isu0bpartnerAttrDf  = spark.sql(f"select 'ISU' as sourceSystemCode, \
+                                      a.businessPartnerNumber as businessPartnerGroupNumber, \
+                                      a.validFromDate as validFromDate, \
+                                      a.validToDate as validToDate, \
+                                      a.businessPartnerGroupCode as businessPartnerGroupCode, \
+                                      a.businessPartnerGroup as businessPartnerGroup, \
+                                      a.nameGroup1 as businessPartnerGroupName1, \
+                                      a.nameGroup2 as businessPartnerGroupName2, \
+                                      a.externalBusinessPartnerNumber as externalNumber, \
+                                      a.createdDateTime as createdDateTime, \
+                                      a.createdBy as createdBy, \
+                                      a.changedDateTime as lastUpdatedDateTime, \
+                                      a.changedBy as lastUpdatedBy \
+                                      FROM {ADS_DATABASE_CLEANSED}.isu_0bpartner_attr a \
+                                      where a.businessPartnerCategoryCode = '3' \
+                                      and a._RecordCurrent = 1 \
+                                      and a._RecordDeleted = 0")
+
+    print(f'{isu0bpartnerAttrDf.count():,} rows in isu0bpartnerAttrDf')
+    display(isu0bpartnerAttrDf)
+    
+    crm0bpartnerAttrDf  = spark.sql(f"select b.paymentAssistSchemeIndicator as paymentAssistSchemeFlag, \
+                                      b.billAssistIndicator as billAssistFlag, \
+                                      b.kidneyDialysisIndicator as kidneyDialysisFlag \
+                                      FROM {ADS_DATABASE_CLEANSED}.crm_0bpartner_attr b \
+                                      where b.businessPartnerCategoryCode = '3' \
+                                      and b._RecordCurrent = 1 \
+                                      and b._RecordDeleted = 0")                                    
+
+    print(f'{crm0bpartnerAttrDf.count():,} rows in crm0bpartnerAttrDf')
+    display(crm0bpartnerAttrDf)
+    
+    #Dummy Record to be added to Business Partner Group Dimension
+    dummyDimRecDf = spark.createDataFrame([("ISU", "-1")], ["sourceSystemCode", "businessPartnerNumber"])
+    
+    #3.JOIN TABLES
+    df = isu0bpartnerAttrDf.join(crm0bpartnerAttrDf, isu0bpartnerAttrDf.businessPartnerNumber == crm0bpartnerAttrDf.businessPartnerNumber, how="left")\
+                            .drop(crm0bpartnerAttrDf.businessPartnerNumber)
+    df = df.select("sourceSystemCode","businessPartnerGroupNumber","validFromDate","validToDate", \
+                                                "businessPartnerGroupCode","businessPartnerGroup","businessPartnerGroupName1","businessPartnerGroupName2","externalNumber" \
+                                                "paymentAssistSchemeFlag","billAssistFlag","kidneyDialysisFlag","createdDateTime","createdBy","lastUpdatedDateTime","lastUpdatedBy") 
+    #4.UNION TABLES
+    df = df.unionByName(dummyDimRecDf, allowMissingColumns = True)
+
+    #5.Apply schema definition
+    newSchema = StructType([
+                            StructField('sourceSystemCode', StringType(), True),
+                            StructField('businessPartnerGroupNumber', StringType(), True),
+                            StructField('validFromDate', DateType(), True),
+                            StructField('validToDate', DateType(), True),
+                            StructField('businessPartnerGroupCode', StringType(), True),
+                            StructField('businessPartnerGroup', StringType(), True),
+                            StructField('businessPartnerGroupName1', StringType(), True),
+                            StructField('businessPartnerGroupName2', StringType(), True),
+                            StructField('externalNumber', StringType(), True),
+                            StructField('paymentAssistSchemeFlag', StringType(), True),
+                            StructField('billAssistFlag', StringType(), True),
+                            StructField('kidneyDialysisFlag', StringType(), True),
+                            StructField('createdDateTime', StringType(), True),
+                            StructField('createdBy', StringType(), True),
+                            StructField('lastUpdatedDateTime', TimeStampType(), True),
+                            StructField('lastUpdatedBy', StringType(), True)                        
+                      ])
+
+    df = spark.createDataFrame(df.rdd, schema=newSchema)
+    #5.SELECT / TRANSFORM
+    #df = df.selectExpr( \
+     #"meterId", \
+     #"sourceSystemCode", \
+     #"meterSize", \
+     #"waterType"
+    #)  
+    return df
+  
+
+# COMMAND ----------
+
+
