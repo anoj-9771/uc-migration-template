4eb373b1d38ccbf3d3423f8c778c84ae293a5e2d
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2022-01-12 23:33:52 +0000

    Date Function

--- Databricks/notebooks/includes/util-general.py
+++ Databricks/notebooks/includes/util-general.py
@@ -318,17 +318,20 @@ def GeneralGetDataLoadMode(truncate, upsert, append):
 
 # COMMAND ----------
 
-def GeneralToValidDateTime(dateIn, fmt = ""):
+def GeneralToValidDateTime(dateIn, keyInd ="N", fmt = "" ):
   
   #This function validates the date
   from datetime import datetime
   
-  if dateIn is None: return
-
   dateStr = str(dateIn)
   lowDate = str('19000101000000')
-  highDate = str('20991231000000')
+  highDate = str('20991231000000')    
     
+  if keyInd == "Y" and dateIn is None:
+    return datetime.strptime(lowDate, "%Y%m%d%H%M%S")    
+  elif keyInd == "N" and dateIn is None:
+    return 
+   
   date_formats = ["%Y-%m-%dT%H:%M:%S", "%Y%m%d%I%M%S %p", "%Y%m%d%H%M%S","%d%y%m", "%d%m%Y", "%Y%m%d", "%d-%m-%Y", "%Y-%m-%d"]
 
   if fmt != "" : 
