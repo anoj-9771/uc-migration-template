95a630f87104e361defdddda50c8da139d62d39a
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2022-02-03 05:41:26 +0000

    Meter Changes

--- Databricks/notebooks/curated/common/functions/dimensions/Meter.py
+++ Databricks/notebooks/curated/common/functions/dimensions/Meter.py
@@ -16,44 +16,25 @@ def getMeter():
     #notes for future: a number of column values can be derived with aditional code/queries. LastActivity reason code can only be mapped from meter change reason code/reason after conversion of values. The text is the same but the codes differ
     accessZ309TpropmeterDf = spark.sql(f"select 'ACCESS' as sourceSystemCode, \
                                               row_number() over (order by metermakernumber) as meterNumber, \
-                                              '' as installationID, \
-                                              meterRemovedDate as validToDate, \
-                                              meterRemovedDate as registerToDate, \
-                                              '' as registerNumber, \
                                               coalesce(meterMakerNumber,'') as meterSerialNumber, \
-                                              null as logicalMeterNumber, \
-                                              meterFittedDate as validFromDate, \
                                               null as materialNumber, \
                                               case when meterClass = 'Standpipe' then 'Customer Standpipe' else 'Water Meter' end as usageMeterType, \
                                               meterSize, \
-                                              case when waterMeterType = 'Potable' then 'Drinking Water' \
-                                                   when waterMeterType = 'Recycled' then 'Recycled Water' else waterMeterType end as waterType, \
+                                              case when waterMeterType in ('Potable','Recycled') then waterMeterType else null end as waterType, \
                                               null as meterCategoryCode, \
-                                              meterCategory, \
+                                              null as meterCategory, \
                                               case when meterClass = 'Standpipe' then 'STANDPIPE' \
                                                    when meterGroup = 'Normal Reading' then 'NORMAL' \
                                                    when meterGroup = 'Automated Reading (AMI)' then 'AMI' \
                                                    when meterGroup = 'Remote Reading' then 'AMR' \
                                                    else null end as meterReadingType, \
                                               null as meterDescription, \
+                                              meterFittedDate, \
+                                              meterRemovedDate, \
                                               null as manufacturerName, \
                                               null as manufacturerSerialNumber, \
                                               null as manufacturerModelNumber, \
-                                              case when right(meterSize,2) = 'mm' then 'KL' else 'IN' end as measurementUnit, \
-                                              null as registerFromDate, \
-                                              null as latestActivityReasonCode, \
-                                              null as latestActivityReason, \
                                               null as inspectionRelevanceFlag, \
-                                              null as registerGroupCode, \
-                                              null as registerGroup, \
-                                              null as registerTypeCode, \
-                                              null as registerType, \
-                                              null as registerCategoryCode, \
-                                              null as registerCategory, \
-                                              null as registerIdCode, \
-                                              null as registerId, \
-                                              null as divisionCategoryCode, \
-                                              null as divisionCategory, \
                                               row_number() over (partition by metermakernumber order by meterFittedDate desc) rownum \
                                       from {ADS_DATABASE_CLEANSED}.access_z309_tpropmeter \
                                       where (meterFittedDate <> meterRemovedDate or meterRemovedDate is null) \
@@ -67,17 +48,19 @@ def getMeter():
     accessZ309TpropmeterDf = accessZ309TpropmeterDf.drop(accessZ309TpropmeterDf.rownum)
 
     print(f'{accessZ309TpropmeterDf.count():,} rows in accessZ309TpropmeterDf')
-#     display(accessZ309TpropmeterDf)
+    #display(accessZ309TpropmeterDf)
     #Meter Data from SAP ISU
     isu0ucDeviceAttrDf  = spark.sql(f"select 'ISU' as sourceSystemCode, \
                                       equipmentNumber as meterNumber, \
                                       deviceNumber as meterSerialNumber, \
-                                      a.materialNumber, \
+                                      materialNumber, \
                                       trim(LEADING '0' FROM deviceSize)||' mm' as meterSize, \
                                       assetManufacturerName as manufacturerName, \
                                       manufacturerSerialNumber, \
                                       manufacturerModelNumber, \
-                                      case when inspectionRelevanceIndicator = 'X' then 'Y' else 'N' end as inspectionRelevanceFlag \
+                                      case when inspectionRelevanceIndicator = 'X' then 'Y' else 'N' end as inspectionRelevanceFlag, \
+                                      null as meterFittedDate, \
+                                      null as meterRemovedDate \
                                       from {ADS_DATABASE_CLEANSED}.isu_0uc_device_attr a \
                                       where a._RecordCurrent = 1 \
                                       and a._RecordDeleted = 0 \
@@ -85,23 +68,6 @@ def getMeter():
     
     print(f'{isu0ucDeviceAttrDf.count():,} rows in isu0ucDeviceAttrDf')
 #    display(isu0ucDeviceAttrDf)
-    #save to table for use in subsequent queries
-    isu0ucDeviceAttrDf.createOrReplaceTempView('allMeters')
-
-    isu0ucDeviceHAttrDf  = spark.sql(f"select equipmentNumber as meterNumber, \
-                                        logicalDeviceNumber as logicalMeterNumber, \
-                                        activityReasonCode as latestActivityReasonCode, \
-                                        activityReason as latestActivityReason, \
-                                        registerGroupCode, \
-                                        registerGroup, \
-                                        ToValidDate(validFromDate)as validFromDate, \
-                                        ToValidDate(validToDate,'Mandatory') as validToDate \
-                                      from {ADS_DATABASE_CLEANSED}.isu_0uc_deviceh_attr a \
-                                      where a._RecordCurrent = 1 \
-                                      and a._RecordDeleted = 0")
-    
-    print(f'{isu0ucDeviceHAttrDf.count():,} rows in isu0ucDeviceHAttrDf')
-#     display(isu0ucDeviceHAttrDf)
 
     isu0ucDevCatAttrDf  = spark.sql(f"select distinct a.materialNumber, \
                                         case when functionClassCode = '9000' then 'Customer Standpipe' else 'Water Meter' end as usageMeterType, \
@@ -117,102 +83,26 @@ def getMeter():
                                       and a._RecordDeleted = 0")
     
     print(f'{isu0ucDevCatAttrDf.count():,} rows in isu0ucDevCatAttrDf')
-#     display(isu0ucDevCatAttrDf)
-    isu0ucRegistAttrDf  = spark.sql(f"select equipmentNumber as meterNumber, \
-                                        a.logicalRegisterNumber, \
-                                        unitOfMeasurementMeterReading as measurementUnit, \
-                                        registerNumber, \
-                                        registerTypeCode, \
-                                        registerType, \
-                                        registerCategoryCode, \
-                                        registerCategory, \
-                                        registerIdCode, \
-                                        registerId, \
-                                        divisionCategoryCode, \
-                                        divisionCategory, \
-                                        ToValidDate(a.validFromDate) as registerFromDate, \
-                                        ToValidDate(a.validToDate,'MANDATORY') as registerToDate, \
-                                        b.installationId, \
-                                        ToValidDate(b.validFromDate) as reginstFromDate, \
-                                        ToValidDate(b.validToDate,'MANDATORY') as reginstToDate \
-                                      from {ADS_DATABASE_CLEANSED}.isu_0uc_regist_attr a \
-                                      join {ADS_DATABASE_CLEANSED}.isu_0uc_reginst_str_attr b on a.logicalRegisterNumber = b.logicalRegisterNumber \
-                                      where a._RecordCurrent = 1 and a._RecordDeleted = 0 and nvl(registerNotRelevantToBilling, ' ') <> 'X'")
-	 
-    print(f'{isu0ucRegistAttrDf.count():,} rows in isu0ucRegistAttrDf')
-#     display(isu0ucRegistAttrDf)
-                                    
-#     isu0ucReginstStrAttrDf  = spark.sql(f"select logicalRegisterNumber, \
-#                                       nvl(registerNotRelevantToBilling, ' ') as registerNotRelevantToBilling \
-#                                       from {ADS_DATABASE_CLEANSED}.isu_0uc_reginst_str_attr \
-#                                       where nvl(registerNotRelevantToBilling, ' ') <> 'X' \
-#                                         and _RecordCurrent = 1 and _RecordDeleted = 0 ")
-	 
-#     print(f'{isu0ucReginstStrAttrDf.count():,} rows in isu0ucReginstStrAttrDf')
-# #     display(isu0ucReginstStrAttrDf)                                    
+#     display(isu0ucDevCatAttrDf)  
     
     #3.JOIN TABLES
-    df = isu0ucDeviceAttrDf.join(isu0ucDeviceHAttrDf, isu0ucDeviceHAttrDf.meterNumber == isu0ucDeviceAttrDf.meterNumber, 
-                                  how="leftouter") \
-                            .drop(isu0ucDeviceHAttrDf.meterNumber)
-    print(f'{df.count():,} rows after merge 1')
-#     display(df)
-    
-    df = df.join(isu0ucDevCatAttrDf, isu0ucDevCatAttrDf.materialNumber == df.materialNumber, 
+    df = isu0ucDeviceAttrDf.join(isu0ucDevCatAttrDf, isu0ucDeviceAttrDf.materialNumber == isu0ucDevCatAttrDf.materialNumber, 
                                   how="leftouter") \
                             .drop(isu0ucDevCatAttrDf.materialNumber)
-    print(f'{df.count():,} rows after merge 2')
-#     display(df)
-    df = df.join(isu0ucRegistAttrDf, (isu0ucRegistAttrDf.meterNumber == df.meterNumber) \
-                                   & (df.validFromDate >= isu0ucRegistAttrDf.registerFromDate) \
-                                   & (df.validToDate <= isu0ucRegistAttrDf.registerToDate) \
-                                   & (df.validFromDate >= isu0ucRegistAttrDf.reginstFromDate) \
-                                   & (df.validToDate <= isu0ucRegistAttrDf.reginstToDate), \
-                                  how="leftouter") \
-                            .drop(isu0ucRegistAttrDf.meterNumber)
-    print(f'{df.count():,} rows after merge 3')
-#     display(df)
-    
-#     df = df.join(isu0ucReginstStrAttrDf, (isu0ucReginstStrAttrDf.logicalRegisterNumber == df.logicalRegisterNumber), \
-#                                   how="leftouter") \
-#                             .drop(isu0ucReginstStrAttrDf.logicalRegisterNumber )
-#     print(f'{df.count():,} rows after merge 4')
-                                    
+    print(f'{df.count():,} rows after merge 1')
+    #display(df)
+        
 #     #re-order columns
-    df = df.select('sourceSystemCode','meterNumber','installationID','validToDate','registerToDate','registerNumber','meterSerialNumber','logicalMeterNumber','validFromDate',
-                    'materialNumber','usageMeterType','meterSize','waterType','meterCategoryCode','meterCategory',
-                    'meterReadingType','meterDescription','manufacturerName','manufacturerSerialNumber','manufacturerModelNumber',
-                    'measurementUnit','registerFromDate','latestActivityReasonCode','latestActivityReason','inspectionRelevanceFlag',
-                    'registerGroupCode','registerGroup','registerTypeCode','registerType','registerCategoryCode',
-                    'registerCategory','registerIdCode','registerId','divisionCategoryCode','divisionCategory'
-                  )
+    df = df.select('sourceSystemCode','meterNumber','meterSerialNumber','materialNumber','usageMeterType','meterSize','waterType','meterCategoryCode','meterCategory',
+                    'meterReadingType','meterDescription','meterFittedDate','meterRemovedDate','manufacturerName','manufacturerSerialNumber','manufacturerModelNumber','inspectionRelevanceFlag')
     
-    
+    #4. UNION
     df = accessZ309TpropmeterDf.union(df)
     print(f'{df.count():,} rows after Union 1')
 #     display(df)    
     
-    #Dummy Record to be added to Meter Dimension
-#     ISUDummy = tuple(['ISU','-1',"2099-12-31","2099-12-31",""] + ['Unknown'] * (len(df.columns) - 5)) #this only works as long as all output columns are string
-#     ACCESSDummy = tuple(['ACCESS','-2',"2099-12-31","2099-12-31",""] + ['Unknown'] * (len(df.columns) -5)) #this only works as long as all output columns are string
-#     dummyDimRecDf = spark.createDataFrame([ISUDummy, ACCESSDummy], df.columns)
-
-    dummyDimRecDf = spark.createDataFrame([("ISU","-1","","2099-12-31","2099-12-31","","1900-01-01"),("ACCESS","-2","","2099-12-31","2099-12-31","","1900-01-01")], 
-                                          ["sourceSystemCode", "meterNumber","installationID","validToDate","registerToDate","registerNumber","validFromDate"])    
-    
-
-#     #check key columns for null    
-    df = df.withColumn("validToDate", when(df.validToDate.isNull(), "2099-12-31").otherwise(df.validToDate)) \
-           .withColumn("registerToDate", when(df.registerToDate.isNull(), "2099-12-31").otherwise(df.registerToDate)) \
-           .withColumn("registerNumber", when(df.registerNumber.isNull(), " ").otherwise(df.registerNumber)) \
-           .withColumn("installationID", when(df.installationID.isNull(), " ").otherwise(df.installationID))
-        
-    df = df.unionByName(dummyDimRecDf, allowMissingColumns = True)
-    
-    df = df.withColumn("validToDate",df['validToDate'].cast(DateType())) \
-           .withColumn("registerToDate",df['registerToDate'].cast(DateType())) \
-           .withColumn("validFromDate",df['validFromDate'].cast(DateType())) \
-           .withColumn("registerFromDate",df['registerFromDate'].cast(DateType())) 
+    dummyDimRecDf = spark.createDataFrame([("ISU","-1"),("ACCESS","-2")], ["sourceSystemCode", "meterNumber"])   
+    df = df.unionByName(dummyDimRecDf, allowMissingColumns = True)    
     print(f'{df.count():,} rows after Union 2')
 #     display(df)
     
@@ -220,13 +110,7 @@ def getMeter():
     newSchema = StructType([
                             StructField('sourceSystemCode', StringType(), False),
                             StructField('meterNumber', StringType(), False),
-                            StructField('installationID', StringType(), False),
-                            StructField('validToDate', DateType(), False),
-                            StructField('registerToDate', DateType(), False),
-                            StructField('registerNumber', StringType(), False),
                             StructField('meterSerialNumber', StringType(), True),
-                            StructField('logicalMeterNumber', StringType(), True),
-                            StructField('validFromDate', DateType(), True),
                             StructField('materialNumber', StringType(), True),
                             StructField('usageMeterType', StringType(), True),
                             StructField('meterSize', StringType(), True),
@@ -235,67 +119,17 @@ def getMeter():
                             StructField('meterCategory', StringType(), True),
                             StructField('meterReadingType', StringType(), True),
                             StructField('meterDescription', StringType(), True),
+                            StructField('meterFittedDate', DateType(), True),
+                            StructField('meterRemovedDate', DateType(), True),
                             StructField('manufacturerName', StringType(), True),
                             StructField('manufacturerSerialNumber', StringType(), True),
                             StructField('manufacturerModelNumber', StringType(), True),
-                            StructField('measurementUnit', StringType(), True),
-                            StructField('registerFromDate', DateType(), True),                                    
-                            StructField('latestActivityReasonCode', StringType(), True),
-                            StructField('latestActivityReason', StringType(), True),
-                            StructField('inspectionRelevanceFlag', StringType(), True),
-                            StructField('registerGroupCode', StringType(), True),
-                            StructField('registerGroup', StringType(), True),
-                            StructField('registerTypeCode', StringType(), True),
-                            StructField('registerType', StringType(), True),
-                            StructField('registerCategoryCode', StringType(), True),
-                            StructField('registerCategory', StringType(), True),
-                            StructField('registerIdCode', StringType(), True),
-                            StructField('registerId', StringType(), True),
-                            StructField('divisionCategoryCode', StringType(), True),
-                            StructField('divisionCategory', StringType(), True)                        
-                      ])
-
+                            StructField('inspectionRelevanceFlag', StringType(), True),   
+                      ])    
+    
     df = spark.createDataFrame(df.rdd, schema=newSchema)
-#     display(df)
-    #6.SELECT / TRANSFORM
-    df = df.selectExpr( \
-     "sourceSystemCode", \
-     "meterNumber", \
-     "installationID", \
-     "meterSerialNumber", \
-     "logicalMeterNumber", \
-     "validFromDate", \
-     "validToDate", \
-     "materialNumber", \
-     "usageMeterType", \
-     "meterSize", \
-     "waterType", \
-     "meterCategoryCode", \
-     "meterCategory", \
-     "meterReadingType", \
-     "meterDescription", \
-     "manufacturerName", \
-     "manufacturerSerialNumber", \
-     "manufacturerModelNumber", \
-     "measurementUnit", \
-     "registerFromDate", \
-     "registerToDate", \
-     "latestActivityReasonCode", \
-     "latestActivityReason", \
-     "inspectionRelevanceFlag", \
-     "registerNumber", \
-     "registerGroupCode", \
-     "registerGroup", \
-     "registerTypeCode", \
-     "registerType", \
-     "registerCategoryCode", \
-     "registerCategory", \
-     "registerIdCode", \
-     "registerId", \
-     "divisionCategoryCode", \
-     "divisionCategory"
-    )  
 #     display(df)    
+    
     return df
   
 
