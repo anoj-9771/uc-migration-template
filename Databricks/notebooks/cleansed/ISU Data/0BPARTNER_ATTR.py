71b9e75db812bd505e2bd7c85e75630cd0518aa4
otob@sydneywater.com.au <otob@sydneywater.com.au>
2022-02-04 00:07:34 +0000

    notebook schema fix

--- Databricks/notebooks/cleansed/ISU Data/0BPARTNER_ATTR.py
+++ Databricks/notebooks/cleansed/ISU Data/0BPARTNER_ATTR.py
@@ -244,7 +244,6 @@ df_cleansed = spark.sql(f"SELECT \
                                LEFT OUTER JOIN {ADS_DATABASE_CLEANSED}.isu_TSAD3T TITLE ON BP.TITLE = TITLE.titlecode \
                                                                               AND TITLE._RecordDeleted = 0 AND TITLE._RecordCurrent = 1")
 
-display(df_cleansed)
 print(f'Number of rows: {df_cleansed.count()}')
 
 # COMMAND ----------
@@ -310,15 +309,13 @@ newSchema = StructType(
     StructField('_RecordCurrent',IntegerType(),False)
   ]
 )
-# Apply the new schema to cleanse Data Frame
-df_updated_column = spark.createDataFrame(df_cleansed.rdd, schema=newSchema)
-display(df_updated_column)
+
 
 # COMMAND ----------
 
 # DBTITLE 1,12. Save Data frame into Cleansed Delta table (Final)
 #Save Data frame into Cleansed Delta table (final)
-DeltaSaveDataframeDirect(df_updated_column, source_group, target_table, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", "")
+DeltaSaveDataframeDirect(df_cleansed, source_group, target_table, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", newSchema, "")
 
 # COMMAND ----------
 
