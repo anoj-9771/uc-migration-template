0267c6eee2db71a5435bbc1d075440c76eab19a3
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2022-02-04 00:14:39 +0000

    NB change

--- Databricks/notebooks/cleansed/CRM Data/0BP_ID_ATTR.py
+++ Databricks/notebooks/cleansed/CRM Data/0BP_ID_ATTR.py
@@ -197,7 +197,6 @@ df_cleansed = spark.sql(f"SELECT \
                            LEFT OUTER JOIN {ADS_DATABASE_CLEANSED}.crm_0BP_ID_TYPE_TEXT BP_TXT \
                                     ON BP.TYPE = BP_TXT.identificationTypeCode AND BP_TXT._RecordDeleted = 0 AND BP_TXT._RecordCurrent = 1")
 
-display(df_cleansed)
 print(f'Number of rows: {df_cleansed.count()}')
 
 # COMMAND ----------
@@ -221,14 +220,11 @@ newSchema = StructType([
 	StructField('_RecordCurrent',IntegerType(),False)
 ])
 
-df_updated_column = spark.createDataFrame(df_cleansed.rdd, schema=newSchema)
-
-
 # COMMAND ----------
 
 # DBTITLE 1,12. Save Data frame into Cleansed Delta table (Final)
 #Save Data frame into Cleansed Delta table (final)
-DeltaSaveDataframeDirect(df_updated_column, source_group, target_table, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", "")
+DeltaSaveDataframeDirect(df_cleansed, source_group, target_table, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", newSchema, "")
 
 # COMMAND ----------
 
