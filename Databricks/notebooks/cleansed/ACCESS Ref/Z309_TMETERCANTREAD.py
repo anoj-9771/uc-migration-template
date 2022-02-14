a67ced7a1bf4ecd5f169144bbabc3cef3af89915
o3hz@sydneywater.com.au <o3hz@sydneywater.com.au>
2022-02-14 23:46:53 +0000

    Verify Schema function call fix

--- Databricks/notebooks/cleansed/ACCESS Ref/Z309_TMETERCANTREAD.py
+++ Databricks/notebooks/cleansed/ACCESS Ref/Z309_TMETERCANTREAD.py
@@ -196,7 +196,6 @@ df_cleansed = spark.sql(f"SELECT case when C_METE_CANT_READ = 'na' then '' else 
 	FROM {ADS_DATABASE_STAGE}.{source_object} \
          ")
 
-display(df_cleansed)
 print(f'Number of rows: {df_cleansed.count()}')                        
 
 # COMMAND ----------
@@ -213,15 +212,12 @@ newSchema = StructType([
 	StructField('_RecordCurrent',IntegerType(),False)
 ])
 
-df_updated_column = spark.createDataFrame(df_cleansed.rdd, schema=newSchema)
-display(df_updated_column)
-print(f'Number of rows: {df_updated_column.count()}')
 
 # COMMAND ----------
 
 # DBTITLE 1,12. Save Data frame into Cleansed Delta table (Final)
 #Save Data frame into Cleansed Delta table (final)
-DeltaSaveDataframeDirect(df_updated_column, source_group, target_table, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", "")
+DeltaSaveDataframeDirect(df_cleansed, source_group, target_table, ADS_DATABASE_CLEANSED, ADS_CONTAINER_CLEANSED, "overwrite", newSchema, "")
 
 # COMMAND ----------
 
