719a881b912bb601db109ca93093fb7e9da0fca1
Pavan Parameswar <ov8z@sydneywater.com.au>
2022-04-20 15:23:31 +1000

    bus rec changes

--- ADS-Databases/CntrlDb/CTL/Stored Procedures/GetBusinessRecCurated.sql
+++ ADS-Databases/CntrlDb/CTL/Stored Procedures/GetBusinessRecCurated.sql
@@ -8,13 +8,14 @@ SELECT
 	   recLog.BusinessRecId,
 	   recLog.CreatedBatchExecutionId,
 	   recLog.CreatedTaskExecutionLogId     
-  FROM (select *, row_number() over (partition by BusinessReconGroup, MeasureId order by CreatedDateTime desc) as CurrentRec
+  FROM (select *, row_number() over (partition by BusinessReconGroup, MeasureId, MeasureName order by CreatedDateTime desc) as CurrentRec
         from CTL.BusinessRecCurated
         where (CuratedPipelineRunID is null or BusinessRecResult is null)
         ) recLog,
        CTL.BusinessRecConfig recMstr
  where recLog.BusinessReconGroup = recMstr.BusinessReconGroup
    AND recLog.MeasureId = recMstr.MeasureId
+   and recLog.MeasureName = recMstr.MeasureName
    AND recMstr.Enabled = 1
    AND recLog.CurrentRec = 1
    AND recMstr.BusinessReconGroup = @BusinessReconGroup
