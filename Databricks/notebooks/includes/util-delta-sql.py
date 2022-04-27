c42148dddabf60c1e4beb76c683e954662b4e1d2
Narayana Rao <9he@sydneywater.com.au>
2022-04-27 14:08:08 +1000

    Renamed all *new method names to original method names

--- Databricks/notebooks/includes/util-delta-sql.py
+++ Databricks/notebooks/includes/util-delta-sql.py
@@ -528,7 +528,7 @@ def DeltaSyncToSQLDWOverwrite(delta_table, target_schema, target_table):
 
 # COMMAND ----------
 
-def DeltaSaveDataFrameToDeltaTableNew(
+def DeltaSaveDataFrameToDeltaTable(
   dataframe, target_table, target_data_lake_zone, target_database, data_lake_folder, data_load_mode, schema, track_changes = False, is_delta_extract = False, business_key = "", AddSKColumn = False, delta_column = "", start_counter = "0", end_counter = "0"):
   
   stage_table_name = f"{ADS_DATABASE_STAGE}.{target_table}"
@@ -544,7 +544,7 @@ def DeltaSaveDataFrameToDeltaTableNew(
   LogEtl(f"write to stg table2")
   
   #Use our generic method to save the dataframe now to Delta Table
-  DeltaSaveToDeltaTableNew(
+  DeltaSaveToDeltaTable(
     source_table = stage_table_name, 
     target_table = target_table, 
     target_data_lake_zone = target_data_lake_zone, 
@@ -568,7 +568,7 @@ def DeltaSaveDataFrameToDeltaTableNew(
 
 # COMMAND ----------
 
-def DeltaSaveToDeltaTableNew(
+def DeltaSaveToDeltaTable(
   source_table, target_table, target_data_lake_zone, target_database, data_lake_folder, data_load_mode, track_changes = False, is_delta_extract = False, business_key = "", delta_column = "", start_counter = "0", end_counter = "0"):
   
   '''
@@ -597,7 +597,7 @@ def DeltaSaveToDeltaTableNew(
   
   #Generate the Query to save the data
   LogEtl("Generating delta SQL query")
-  delta_query = SQLMerge_DeltaTable_GenerateSQLNew(
+  delta_query = SQLMerge_DeltaTable_GenerateSQL(
       source_table_name = source_table, 
       target_table_name = target_table_fqn, 
       business_key = business_key, 
