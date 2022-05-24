2b216a7fda61282b7cf3598841bb3ba09bfd03bd
o9he@sydneywater.com.au <o9he@sydneywater.com.au>
2022-05-24 09:58:02 +0000

    sourcesystemcode removed

--- Databricks/notebooks/curated/03 bridgeTables/BusinessPartnerGroupRelationship.py
+++ Databricks/notebooks/curated/03 bridgeTables/BusinessPartnerGroupRelationship.py
@@ -44,28 +44,24 @@ def getBusinessPartnerGroupRelationship():
                                       from {ADS_DATABASE_CURATED}.dimBusinessPartnerGroup \
                                       where _RecordCurrent = 1 and _RecordDeleted = 0") 
     
-    dummyDimRecDf = spark.sql(f"select businessPartnerSK as dummyDimSK, sourceSystemCode, 'dimBusinessPartner' as dimension from {ADS_DATABASE_CURATED}.dimBusinessPartner where businessPartnerNumber = '-1' \
-                            union select businessPartnerGroupSK as dummyDimSK, sourceSystemCode, 'dimBusinessPartnerGroup' as dimension from {ADS_DATABASE_CURATED}.dimBusinessPartnerGroup \
+    dummyDimRecDf = spark.sql(f"select businessPartnerSK as dummyDimSK, 'dimBusinessPartner' as dimension from {ADS_DATABASE_CURATED}.dimBusinessPartner where businessPartnerNumber = '-1' \
+                            union select businessPartnerGroupSK as dummyDimSK, 'dimBusinessPartnerGroup' as dimension from {ADS_DATABASE_CURATED}.dimBusinessPartnerGroup \
                                                                                                                                             where businessPartnerGroupNumber = '-1'")
     
     
     #2.Joins to derive SKs
-    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dim0bpartnerDf, (isu0bpRelationsAttrDf.businessPartnerNumber == dim0bpartnerDf.businessPartnerNumber) \
-                               & (isu0bpRelationsAttrDf.sourceSystemCode == dim0bpartnerDf.sourceSystemCode), how="left") \
+    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dim0bpartnerDf, (isu0bpRelationsAttrDf.businessPartnerNumber == dim0bpartnerDf.businessPartnerNumber), how="left") \
                     .select(isu0bpRelationsAttrDf['*'], dim0bpartnerDf['businessPartnerSK'])    
     
-    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dim0bpGroupDf, (isu0bpRelationsAttrDf.businessPartnerGroupNumber == dim0bpGroupDf.businessPartnerGroupNumber) \
-                               & (isu0bpRelationsAttrDf.sourceSystemCode == dim0bpGroupDf.sourceSystemCode), how="left") \
+    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dim0bpGroupDf, (isu0bpRelationsAttrDf.businessPartnerGroupNumber == dim0bpGroupDf.businessPartnerGroupNumber), how="left") \
                     .select(isu0bpRelationsAttrDf['*'], dim0bpGroupDf['businessPartnerGroupSK'])
     
     #2.Joins to derive SKs of dummy dimension(-1) records, to be used when the lookup fails for dimensionSk
   
-    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dummyDimRecDf, (dummyDimRecDf.dimension == 'dimBusinessPartner') \
-                               & (isu0bpRelationsAttrDf.sourceSystemCode == dummyDimRecDf.sourceSystemCode), how="left") \
+    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dummyDimRecDf, (dummyDimRecDf.dimension == 'dimBusinessPartner'), how="left") \
                     .select(isu0bpRelationsAttrDf['*'], dummyDimRecDf['dummyDimSK'].alias('dummyBusinessPartnerSK'))
     
-    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dummyDimRecDf, (dummyDimRecDf.dimension == 'dimBusinessPartnerGroup') \
-                               & (isu0bpRelationsAttrDf.sourceSystemCode == dummyDimRecDf.sourceSystemCode), how="left") \
+    isu0bpRelationsAttrDf = isu0bpRelationsAttrDf.join(dummyDimRecDf, (dummyDimRecDf.dimension == 'dimBusinessPartnerGroup'), how="left") \
                     .select(isu0bpRelationsAttrDf['*'], dummyDimRecDf['dummyDimSK'].alias('dummyBusinessPartnerGroupSK'))
     
     #3.UNION TABLES    
