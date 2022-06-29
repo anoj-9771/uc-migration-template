ba8c970ffd384d6d270df70eed985c02aab0fc7d
o9he@sydneywater.com.au <o9he@sydneywater.com.au>
2022-06-29 06:40:42 +0000

    business key change to align with the source business keys as per mapping doc v1.10

--- Databricks/notebooks/curated/01 dimensions/Contract.py
+++ Databricks/notebooks/curated/01 dimensions/Contract.py
@@ -19,7 +19,8 @@
 def getContract():
 
     #1.Load current Cleansed layer table data into dataframe
-    df = spark.sql(f"select  co.contractId, \
+    df = spark.sql(f"with contract as ( \
+                   select  co.contractId, \
                              coalesce(coh.validFromDate,to_date('1900-01-01','yyyy-MM-dd')) as validFromDate, \
                              coh.validToDate, \
                              'ISU' as sourceSystemCode, \
@@ -31,7 +32,8 @@ def getContract():
                              ca.contractAccountNumber, \
                              ca.contractAccountCategory, \
                              ca.applicationArea, \
-                             co.installationId \
+                             co.installationId, \
+                             row_number() OVER (PARTITION BY co.contractId,coh.validToDate ORDER BY coh.validToDate desc) as rank \
                              from {ADS_DATABASE_CLEANSED}.isu_0UCCONTRACT_ATTR_2 co left outer join \
                                   {ADS_DATABASE_CLEANSED}.isu_0UCCONTRACTH_ATTR_2 coh on co.contractId = coh.contractId \
                                                            and coh.deletedIndicator is null and coh._RecordDeleted = 0 and coh._RecordCurrent = 1 left outer join \
@@ -39,7 +41,9 @@ def getContract():
                                                            and ca._RecordDeleted = 0 and ca._RecordCurrent = 1 \
                              where co._RecordDeleted = 0 \
                              and   co._RecordCurrent = 1 \
-                     ")
+                          ) \
+                   select * from contract where rank=1 \
+                     ").drop("rank")
 
     df.createOrReplaceTempView('allcontracts')
     #2.JOIN TABLES  
@@ -92,7 +96,7 @@ def getContract():
 # COMMAND ----------
 
 df, schema = getContract()
-TemplateEtl(df,  entity="dimContract", businessKey="contractId,validFromDate", schema=schema, writeMode=ADS_WRITE_MODE_MERGE, AddSK=True)
+TemplateEtl(df,  entity="dimContract", businessKey="contractId,validToDate", schema=schema, writeMode=ADS_WRITE_MODE_MERGE, AddSK=True)
 
 # COMMAND ----------
 
