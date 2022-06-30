2d8f315bc2aa25ef69d7fed72f40ccd7a2ca5066
o9he@sydneywater.com.au <o9he@sydneywater.com.au>
2022-06-30 00:13:28 +0000

    Removed the row_number logic to align with the mapping doc changes

--- Databricks/notebooks/curated/01 dimensions/Contract.py
+++ Databricks/notebooks/curated/01 dimensions/Contract.py
@@ -19,8 +19,7 @@
 def getContract():
 
     #1.Load current Cleansed layer table data into dataframe
-    df = spark.sql(f"with contract as ( \
-                   select  co.contractId, \
+    df = spark.sql(f"select  co.contractId, \
                              coalesce(coh.validFromDate,to_date('1900-01-01','yyyy-MM-dd')) as validFromDate, \
                              coh.validToDate, \
                              'ISU' as sourceSystemCode, \
@@ -32,8 +31,7 @@ def getContract():
                              ca.contractAccountNumber, \
                              ca.contractAccountCategory, \
                              ca.applicationArea, \
-                             co.installationId, \
-                             row_number() OVER (PARTITION BY co.contractId,coh.validToDate ORDER BY coh.validToDate desc) as rank \
+                             co.installationId \
                              from {ADS_DATABASE_CLEANSED}.isu_0UCCONTRACT_ATTR_2 co left outer join \
                                   {ADS_DATABASE_CLEANSED}.isu_0UCCONTRACTH_ATTR_2 coh on co.contractId = coh.contractId \
                                                            and coh.deletedIndicator is null and coh._RecordDeleted = 0 and coh._RecordCurrent = 1 left outer join \
@@ -41,9 +39,7 @@ def getContract():
                                                            and ca._RecordDeleted = 0 and ca._RecordCurrent = 1 \
                              where co._RecordDeleted = 0 \
                              and   co._RecordCurrent = 1 \
-                          ) \
-                   select * from contract where rank=1 \
-                     ").drop("rank")
+                     ")
 
     df.createOrReplaceTempView('allcontracts')
     #2.JOIN TABLES  
@@ -51,10 +47,10 @@ def getContract():
     #3.UNION TABLES
     #Create dummy record
     
-    dummyDimRecDf = spark.createDataFrame([("-1","1900-01-01")], ["contractId", "validFromDate"])
+    dummyDimRecDf = spark.createDataFrame([("-1","1900-01-01", "9999-12-31")], ["contractId", "validFromDate", "validToDate"])
                                      
     df = df.unionByName(dummyDimRecDf,allowMissingColumns = True)
-    df = df.withColumn("validFromDate",col("validFromDate").cast("date"))
+    df = df.withColumn("validFromDate",col("validFromDate").cast("date")).withColumn("validToDate",col("validToDate").cast("date"))
     
     #4.SELECT / TRANSFORM
     df = df.selectExpr( \
