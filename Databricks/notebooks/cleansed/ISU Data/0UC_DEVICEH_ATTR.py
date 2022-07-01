caf01526f0678006aa7b70f37d2f54d225124190
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2022-07-01 00:17:20 +0000

    Tech Rec Changes

--- Databricks/notebooks/cleansed/ISU Data/0UC_DEVICEH_ATTR.py
+++ Databricks/notebooks/cleansed/ISU Data/0UC_DEVICEH_ATTR.py
@@ -284,7 +284,6 @@ spark.sql(f" \
     and isu_0UC_DEVICEH_ATTR.validToDate = isu_device_deleted_records.BIS \
     WHEN MATCHED THEN UPDATE SET \
     _DLCleansedZoneTimeStamp = cast('{CurrentTimeStamp}' as TimeStamp) \
-    ,_RecordEnd = cast('{CurrentTimeStamp}' as TimeStamp) \
     ,_RecordDeleted=1 \
     ,_RecordCurrent=0 \
     ")
