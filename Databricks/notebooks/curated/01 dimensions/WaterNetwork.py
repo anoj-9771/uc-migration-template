e475f865240f1b03cc46c417eb6b34165e2c8a36
o13w@sydneywater.com.au <o13w@sydneywater.com.au>
2022-08-10 03:29:19 +0000

    default value chnage to Unknown

--- Databricks/notebooks/curated/01 dimensions/WaterNetwork.py
+++ Databricks/notebooks/curated/01 dimensions/WaterNetwork.py
@@ -22,7 +22,7 @@ def getWaterNetwork():
     baseDf = spark.sql(f"select level30 as deliverySystem, \
                                 level40 as distributionSystem, \
                                 level50 as supplyZone, \
-                                coalesce(level60,'n/a')  as pressureArea, \
+                                coalesce(level60,'Unknown')  as pressureArea, \
                                 case when product = 'Water'  then 'Y' else 'N' end as isPotableWaterNetwork, \
                                 case when product = 'RecycledWater'  then 'Y' else 'N' end as isRecycledWaterNetwork \
                         from {ADS_DATABASE_CLEANSED}.hydra_TSYSTEMAREA \
