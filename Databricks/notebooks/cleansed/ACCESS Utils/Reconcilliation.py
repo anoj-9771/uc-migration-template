b4390d8a82d3490f06919d8454f8d3251b95af5f
KELVIN HONG <o3bj@sydneywater.com.au>
2022-09-20 23:02:09 +0000

    Merge pull request 1562 from uc1-release-11 into scd-uc1-rc1

---
+++ Databricks/notebooks/cleansed/ACCESS Utils/Reconcilliation.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+dftbl = sqlContext.sql("show tables from cleansed")
+dftbl2 = dftbl.where(col('tableName').like('%z309%'))
+
+tmplist = []
+for row in dftbl2.rdd.collect():
+    try:
+        tmp = 'select count(*) myrowcnt from ' + row['database'] + '.' + row['tableName']
+        tmpdf = sqlContext.sql(tmp)
+        myrowcnt= tmpdf.collect()[0]['myrowcnt'] 
+        tmplist.append((row['database'], row['tableName'],myrowcnt))
+    except:
+        tmplist.append((row['database'], row['tableName'],-1))
+
+columns =  ['database', 'tableName', 'rowCount']     
+df = spark.createDataFrame(tmplist, columns)    
+display(df)
+
+# COMMAND ----------
+
+display(dftbl)
+
+# COMMAND ----------
+
+
