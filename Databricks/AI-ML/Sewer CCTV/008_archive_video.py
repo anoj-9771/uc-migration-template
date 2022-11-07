1519201a07cbdda2fe956d067cd8fe886a0398a3
onyu@sydneywater.com.au <onyu@sydneywater.com.au>
2022-11-07 03:15:46 +0000

    Fix drop duplicates bug

--- Databricks/AI-ML/Sewer CCTV/008_archive_video.py
+++ Databricks/AI-ML/Sewer CCTV/008_archive_video.py
@@ -14,47 +14,64 @@ _PRIORITY = dbutils.widgets.get("priority")
 #data inserted to the video metadata table
 df_cctv_metadata = (spark.table("stage.cctv_video_metadata")
                     .where(psf.col("video_id") == _VIDEO_ID)
+                    .drop("_DLRawZoneTimeStamp")
                     .dropDuplicates()
+                    .withColumn("_DLRawZoneTimeStamp", psf.current_timestamp())  #timestamp for when the data has been processed
                    )
 df_cctv_metadata.write.mode("append").insertInto('raw.cctv_video_metadata')
 
 #4. ----- Data inserted to the cctv video frames table -----
 df_raw_images = (spark.table("stage.cctv_video_frames")
-                    .where(psf.col("video_id") == _VIDEO_ID)
-                    .dropDuplicates()
+                 .where(psf.col("video_id") == _VIDEO_ID)
+                 .drop("_DLRawZoneTimeStamp")
+                 .dropDuplicates()
+                 .withColumn("_DLRawZoneTimeStamp", psf.current_timestamp())  #timestamp for when the data has been processed
                    )
 df_raw_images.write.format("delta").insertInto('raw.cctv_video_frames')
 
 #data inserted to the raw ocr extract table
 df_raw_ocr = (spark.table("stage.cctv_ocr_extract")
-                    .where(psf.col("video_id") == _VIDEO_ID)
-                    .dropDuplicates()
-                   )
+              .where(psf.col("video_id") == _VIDEO_ID)
+              .drop("_DLRawZoneTimeStamp")
+              .dropDuplicates()
+              .withColumn("_DLRawZoneTimeStamp", psf.current_timestamp())  #timestamp for when the data has been processed
+             )
+
 df_raw_ocr.write.mode("append").insertInto('raw.cctv_ocr_extract')
 
 df_cleansed_ocr = (spark.table("stage.cctv_ocr_extract_cleansed")
-                    .where(psf.col("video_id") == _VIDEO_ID)
-                    .dropDuplicates()
+                   .where(psf.col("video_id") == _VIDEO_ID)
+                   .drop("_DLCleansedZoneTimestamp")
+                   .dropDuplicates()
+                   .withColumn("_DLCleansedZoneTimestamp", psf.current_timestamp())  #timestamp for when the data has been processed
                    )
 
 df_cleansed_ocr.write.mode("append").insertInto('cleansed.cctv_ocr_extract')
 
 df_contractor_annotations = (spark.table("stage.cctv_contractor_annotations")
-                    .where(psf.col("video_id") == _VIDEO_ID)
-                    .dropDuplicates()
+                   .where(psf.col("video_id") == _VIDEO_ID)
+                   .drop("_DLCleansedZoneTimestamp")
+                   .dropDuplicates()
+                   .withColumn("_DLCleansedZoneTimestamp", psf.current_timestamp())  #timestamp for when the data has been processed
                    )
+
 df_contractor_annotations.write.insertInto("cleansed.cctv_contractor_annotations") #contractor identified defects
 
 df_image_classifications = (spark.table("stage.cctv_ai_image_classifications")
                     .where(psf.col("video_id") == _VIDEO_ID)
-                    .dropDuplicates()
+                   .drop("_DLRawZoneTimeStamp")
+                   .dropDuplicates()
+                   .withColumn("_DLRawZoneTimeStamp", psf.current_timestamp())  #timestamp for when the data has been processed
                    )
 df_image_classifications.write.mode("append").insertInto('raw.cctv_ai_image_classifications')
 
 df_cctv_defect_series = (spark.table("stage.cctv_group_ai_identified_defects")
                     .where(psf.col("video_id") == _VIDEO_ID)
-                    .dropDuplicates()
+                   .drop("_DLCleansedZoneTimestamp")
+                   .dropDuplicates()
+                   .withColumn("_DLCleansedZoneTimestamp", psf.current_timestamp())  #timestamp for when the data has been processed
                    )
+
 df_cctv_defect_series.write.insertInto("cleansed.cctv_group_ai_identified_defects")
 
 # COMMAND ----------
