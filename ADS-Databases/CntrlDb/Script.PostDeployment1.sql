2443cc0da2720be0b6940362182fb9c7bcb6aafb
Anthony Xar <o4qu@sydneywater.com.au>
2022-11-08 10:05:54 +1100

    Add Post Deployment Config Data

--- ADS-Databases/CntrlDb/Script.PostDeployment1.sql
+++ ADS-Databases/CntrlDb/Script.PostDeployment1.sql
@@ -1,4 +1,27 @@
-﻿-- /*
+﻿DECLARE @SummaryOfChanges TABLE(Change VARCHAR(20));
+MERGE INTO [dbo].[Config] AS TGT
+USING (
+    SELECT *
+    ,CONVERT(DATETIME, CONVERT(DATETIMEOFFSET, GETDATE()) AT TIME ZONE 'AUS Eastern Standard Time') [CreatedDTS]
+    FROM (
+        SELECT 'IngestionDefault' [KeyGroup], 'RawPath' [Key], '/raw/$SYSTEM$/$SCHEMA$_$TABLE$/$yyyy$/$MM$/$dd$/$HH$$mm$/$guid$.$EXT$' [Value]
+        UNION SELECT 'IngestionDefault' [KeyGroup], 'CleansedPath' [Key], '/cleansed/$SYSTEM$/$SCHEMA$_$TABLE$/' [Value]
+        UNION SELECT 'IngestionDefault' [KeyGroup], 'SourceKeyVaultSecret' [Key], 'daf-sa-lake-connectionstring' [Value]
+        UNION SELECT 'IngestionDefault' [KeyGroup], 'Enabled' [Key], '1' [Value]
+        UNION SELECT 'IngestionSheet' [KeyGroup], 'Index' [Key], '0' [Value]
+        UNION SELECT 'IngestionSheet' [KeyGroup], 'Limit' [Key], '0' [Value]
+        UNION SELECT 'IngestionSheet' [KeyGroup], 'Path' [Key], 'testonly/labware_upload.csv' [Value]
+    ) T
+)
+AS SRC ([KeyGroup],[Key],[Value],[CreatedDTS]) ON TGT.[KeyGroup] = SRC.[KeyGroup] AND TGT.[Key] = SRC.[Key]
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT ([KeyGroup],[Key],[Value],[CreatedDTS]) VALUES ([KeyGroup],[Key],[Value],[CreatedDTS])
+OUTPUT $action 
+    INTO @SummaryOfChanges;
+SELECT * FROM @SummaryOfChanges;
+
+
+-- /*
 -- Post-Deployment Script Template							
 -- --------------------------------------------------------------------------------------
 --  This file contains SQL statements that will be appended to the build script.		
