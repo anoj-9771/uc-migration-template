aee52969519fd06ea593e647b70d2bb15b167979
o7p3@sydneywater.com.au <o7p3@sydneywater.com.au>
2022-11-10 06:13:21 +0000

    Adding new Dimension files for ServiceRequestProcessType and ServiceRequestStatus

---
+++ Databricks/MDP-Framework/Transform/Dimension/ServiceRequestStatus.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+# MAGIC %run ../../Common/common-transform
+
+# COMMAND ----------
+
+def Transform():
+    global df
+    # ------------- TABLES ----------------- #
+    df = GetTable(f"{SOURCE}.crm_tj30t")
+    # ------------- JOINS ------------------ #
+
+    # ------------- TRANSFORMS ------------- #
+    _.Transforms = [
+        f"statusProfile||statusCode {BK}"
+        ,"statusProfile statusProfile"
+        ,"statusCode statusCode"
+        ,"statusShortDescription statusShortDescription"
+        ,"status status"
+    ]
+    df = df.selectExpr(
+        _.Transforms
+    )
+    # ------------- CLAUSES ---------------- #
+
+    # ------------- SAVE ------------------- #
+    #display(df)
+#     CleanSelf()
+    Save(df)
+    #DisplaySelf()
+pass
+Transform()
