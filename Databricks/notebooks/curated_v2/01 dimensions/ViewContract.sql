dc91600cec0d11ea8e4b7bf1ac0506564a430f85
o9he@sydneywater.com.au <o9he@sydneywater.com.au>
2022-11-21 01:57:54 +0000

    Views implementation changes to consider the deletes records

--- Databricks/notebooks/curated_v2/01 dimensions/ViewContract.sql
+++ Databricks/notebooks/curated_v2/01 dimensions/ViewContract.sql
@@ -2,23 +2,39 @@
 -- View: viewContract
 -- Description: viewContract
 CREATE OR REPLACE VIEW curated_v2.viewContract AS
-WITH dateDriver AS
+WITH dateDriverNonDelete AS
 (
     SELECT DISTINCT contractId, _recordStart AS _effectiveFrom FROM curated_v2.dimContract
-    WHERE _recordDeleted <> 1
+    WHERE _recordDeleted = 0
     union
     SELECT DISTINCT contractId, _recordStart AS _effectiveFrom FROM curated_v2.dimContractHistory
-    WHERE _recordDeleted <> 1
+    WHERE _recordDeleted = 0
 ),
-effectiveDateranges AS 
+effectiveDaterangesNonDelete AS 
 (
     SELECT contractId, _effectiveFrom, COALESCE(TIMESTAMP(DATE_ADD(LEAD(_effectiveFrom,1) OVER(PARTITION BY contractId ORDER BY _effectiveFrom), -1)), TIMESTAMP('9999-12-31')) AS _effectiveTo
-    FROM dateDriver
+    FROM dateDriverNonDelete
+),
+dateDriverDelete AS
+(
+    SELECT DISTINCT contractId, _recordStart AS _effectiveFrom FROM curated_v2.dimContract
+    WHERE _recordDeleted = 1
+    union
+    SELECT DISTINCT contractId, _recordStart AS _effectiveFrom FROM curated_v2.dimContractHistory
+    WHERE _recordDeleted = 1
+),
+effectiveDaterangesDelete AS 
+(
+    SELECT contractId, _effectiveFrom, COALESCE(TIMESTAMP(DATE_ADD(LEAD(_effectiveFrom,1) OVER(PARTITION BY contractId ORDER BY _effectiveFrom), -1)), TIMESTAMP('9999-12-31')) AS _effectiveTo
+    FROM dateDriverDelete
 )
+SELECT * FROM 
+(
 SELECT
      dimContract.contractSK
+    ,dimContractHistory.contractHistorySK
     ,dimContract.sourceSystemCode
-    ,dimContract.contractId    
+    ,coalesce(dimContract.contractId, dimContractHistory.contractId, -1) as contractId    
     ,dimContract.companyCode
     ,dimContract.companyName
     ,dimContract.divisionCode
@@ -66,7 +82,6 @@ SELECT
     ,dimContract.createdBy
     ,dimContract.lastChangedDate
     ,dimContract.lastChangedBy
-    ,dimContractHistory.contractHistorySK
     ,dimContractHistory.validFromDate as contractHistoryValidFromDate
     ,dimContractHistory.validToDate as contractHistoryValidToDate
     ,dimContractHistory.CRMProduct
@@ -84,20 +99,116 @@ SELECT
     ,dimContractHistory.isCancelledFlag
     ,effectiveDateRanges._effectiveFrom
     ,effectiveDateRanges._effectiveTo
-    , CASE
-      WHEN CURRENT_DATE() BETWEEN effectiveDateRanges._effectiveFrom AND effectiveDateRanges._effectiveTo then 'Y'
-      ELSE 'N'
-      END AS currentIndicator
-FROM effectiveDateRanges
+    ,dimContract._recordDeleted as _dimContractRecordDeleted
+    ,dimContractHistory._recordDeleted as _dimContractHistoryRecordDeleted
+    ,dimContract._recordCurrent as _dimContractRecordCurrent
+    ,dimContractHistory._recordCurrent as _dimContractHistoryRecordCurrent
+    ,dimContract._recordStart as _dimContractRecordStart
+    ,dimContract._recordEnd as _dimContractRecordEnd
+    ,dimContractHistory._recordStart as _dimContractHistoryRecordStart
+    ,dimContractHistory._recordEnd as _dimContractHistoryRecordEnd
+FROM effectiveDaterangesNonDelete as effectiveDateRanges
 LEFT OUTER JOIN curated_v2.dimContract
     ON dimContract.contractId = effectiveDateRanges.contractId 
         AND dimContract._recordEnd >= effectiveDateRanges._effectiveFrom 
         AND dimContract._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimContract._recordDeleted = 0
 LEFT OUTER JOIN curated_v2.dimContractHistory
     ON dimContractHistory.contractId = effectiveDateRanges.contractId 
         AND dimContractHistory._recordEnd >= effectiveDateRanges._effectiveFrom 
         AND dimContractHistory._recordStart <= effectiveDateRanges._effectiveTo
-ORDER BY effectiveDateRanges._effectiveFrom
+        AND dimContractHistory._recordDeleted = 0
+UNION
+SELECT
+     dimContract.contractSK
+    ,dimContractHistory.contractHistorySK
+    ,dimContract.sourceSystemCode
+    ,coalesce(dimContract.contractId, dimContractHistory.contractId, -1) as contractId   
+    ,dimContract.companyCode
+    ,dimContract.companyName
+    ,dimContract.divisionCode
+    ,dimContract.division
+    ,dimContract.installationNumber
+    ,dimContract.contractAccountNumber
+    ,dimContract.accountDeterminationCode
+    ,dimContract.accountDetermination
+    ,dimContract.allowableBudgetBillingCyclesCode
+    ,dimContract.allowableBudgetBillingCycles
+    ,dimContract.invoiceContractsJointlyCode
+    ,dimContract.invoiceContractsJointly
+    ,dimContract.manualBillContractflag
+    ,dimContract.billBlockingReasonCode
+    ,dimContract.billBlockingReason
+    ,dimContract.specialMoveOutCaseCode
+    ,dimContract.specialMoveOutCase
+    ,dimContract.contractText
+    ,dimContract.legacyMoveInDate
+    ,dimContract.numberOfCancellations
+    ,dimContract.numberOfRenewals
+    ,dimContract.personnelNumber
+    ,dimContract.contractNumberLegacy
+    ,dimContract.isContractInvoicedFlag
+    ,dimContract.isContractTransferredFlag
+    ,dimContract.outsortingCheckGroupForBilling
+    ,dimContract.manualOutsortingCount
+    ,dimContract.serviceProvider
+    ,dimContract.contractTerminatedForBillingFlag
+    ,dimContract.invoicingParty
+    ,dimContract.cancellationReasonCRM
+    ,dimContract.moveInDate
+    ,dimContract.moveOutDate
+    ,dimContract.budgetBillingStopDate
+    ,dimContract.premise
+    ,dimContract.propertyNumber
+    ,dimContract.validFromDate
+    ,dimContract.agreementNumber
+    ,dimContract.addressNumber
+    ,dimContract.alternativeAddressNumber
+    ,dimContract.identificationNumber
+    ,dimContract.objectReferenceIndicator
+    ,dimContract.objectNumber
+    ,dimContract.createdDate
+    ,dimContract.createdBy
+    ,dimContract.lastChangedDate
+    ,dimContract.lastChangedBy
+    ,dimContractHistory.validFromDate as contractHistoryValidFromDate
+    ,dimContractHistory.validToDate as contractHistoryValidToDate
+    ,dimContractHistory.CRMProduct
+    ,dimContractHistory.CRMObjectId
+    ,dimContractHistory.CRMDocumentItemNumber
+    ,dimContractHistory.marketingCampaign
+    ,dimContractHistory.individualContractId
+    ,dimContractHistory.productBeginFlag
+    ,dimContractHistory.productChangeFlag
+    ,dimContractHistory.replicationControlsCode
+    ,dimContractHistory.replicationControls
+    ,dimContractHistory.podUUID
+    ,dimContractHistory.headerTypeCode
+    ,dimContractHistory.headerType
+    ,dimContractHistory.isCancelledFlag
+    ,effectiveDateRanges._effectiveFrom
+    ,effectiveDateRanges._effectiveTo
+    ,dimContract._recordDeleted as _dimContractRecordDeleted
+    ,dimContractHistory._recordDeleted as _dimContractHistoryRecordDeleted
+    ,dimContract._recordCurrent as _dimContractRecordCurrent
+    ,dimContractHistory._recordCurrent as _dimContractHistoryRecordCurrent
+    ,dimContract._recordStart as _dimContractRecordStart
+    ,dimContract._recordEnd as _dimContractRecordEnd
+    ,dimContractHistory._recordStart as _dimContractHistoryRecordStart
+    ,dimContractHistory._recordEnd as _dimContractHistoryRecordEnd
+FROM effectiveDaterangesDelete as effectiveDateRanges
+LEFT OUTER JOIN curated_v2.dimContract
+    ON dimContract.contractId = effectiveDateRanges.contractId 
+        AND dimContract._recordEnd >= effectiveDateRanges._effectiveFrom 
+        AND dimContract._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimContract._recordDeleted = 1
+LEFT OUTER JOIN curated_v2.dimContractHistory
+    ON dimContractHistory.contractId = effectiveDateRanges.contractId 
+        AND dimContractHistory._recordEnd >= effectiveDateRanges._effectiveFrom 
+        AND dimContractHistory._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimContractHistory._recordDeleted = 1
+)
+ORDER BY _effectiveFrom
 
 -- COMMAND ----------
 
--- Databricks/notebooks/curated_v2/01 dimensions/ViewDevice.sql
+++ Databricks/notebooks/curated_v2/01 dimensions/ViewDevice.sql
@@ -2,23 +2,39 @@
 -- View: viewDevice
 -- Description: viewDevice
 CREATE OR REPLACE VIEW curated_v2.viewDevice AS
-WITH dateDriver AS
+WITH dateDriverNonDelete AS
 (
     SELECT DISTINCT deviceNumber, _recordStart AS _effectiveFrom FROM curated_v2.dimDevice
-    WHERE _recordDeleted <> 1
+    WHERE _recordDeleted = 0
     union
     SELECT DISTINCT deviceNumber, _recordStart AS _effectiveFrom FROM curated_v2.dimDeviceHistory
-    WHERE _recordDeleted <> 1
+    WHERE _recordDeleted = 0
 ),
-effectiveDateranges AS 
+effectiveDaterangesNonDelete AS 
 (
     SELECT deviceNumber, _effectiveFrom, COALESCE(TIMESTAMP(DATE_ADD(LEAD(_effectiveFrom,1) OVER(PARTITION BY deviceNumber ORDER BY _effectiveFrom), -1)), '9999-12-31') AS _effectiveTo
-    FROM dateDriver
+    FROM dateDriverNonDelete
+),
+dateDriverDelete AS
+(
+    SELECT DISTINCT deviceNumber, _recordStart AS _effectiveFrom FROM curated_v2.dimDevice
+    WHERE _recordDeleted = 1
+    union
+    SELECT DISTINCT deviceNumber, _recordStart AS _effectiveFrom FROM curated_v2.dimDeviceHistory
+    WHERE _recordDeleted = 1
+),
+effectiveDaterangesDelete AS 
+(
+    SELECT deviceNumber, _effectiveFrom, COALESCE(TIMESTAMP(DATE_ADD(LEAD(_effectiveFrom,1) OVER(PARTITION BY deviceNumber ORDER BY _effectiveFrom), -1)), '9999-12-31') AS _effectiveTo
+    FROM dateDriverDelete
 )
+SELECT * FROM 
+(
 SELECT
      dimdevice.deviceSK
+    ,dimdevicehistory.deviceHistorySK
     ,dimdevice.sourceSystemCode
-    ,dimdevice.deviceNumber
+    ,coalesce(dimdevice.deviceNumber, dimdevicehistory.deviceNumber, -1) as deviceNumber
     ,dimdevice.materialNumber
     ,dimdevice.deviceID
     ,dimdevice.inspectionRelevanceIndicator
@@ -36,7 +52,6 @@ SELECT
     ,dimdevice.ptiNumber
     ,dimdevice.ggwaNumber
     ,dimdevice.certificationRequirementType
-    ,dimdevicehistory.deviceHistorySK
     ,dimdevicehistory.validToDate AS deviceHistoryValidToDate
     ,dimdevicehistory.validFromDate AS deviceHistoryValidFromDate
     ,dimdevicehistory.logicalDeviceNumber
@@ -88,35 +103,150 @@ SELECT
     ,installAttr.division
     ,effectiveDateRanges._effectiveFrom
     ,effectiveDateRanges._effectiveTo
-    , CASE
-      WHEN CURRENT_DATE() BETWEEN effectiveDateRanges._effectiveFrom AND effectiveDateRanges._effectiveTo then 'Y'
-      ELSE 'N'
-      END AS currentIndicator
-FROM effectiveDateRanges
+    ,dimdevice._recordDeleted as _dimDeviceRecordDeleted
+    ,dimdevicehistory._recordDeleted as _dimDeviceHistoryRecordDeleted
+    ,dimdevice._recordCurrent as _dimDeviceRecordCurrent
+    ,dimdevicehistory._recordCurrent as _dimDeviceHistoryRecordCurrent
+FROM effectiveDaterangesNonDelete as effectiveDateRanges
 LEFT OUTER JOIN curated_v2.dimDevice dimdevice
     ON dimdevice.deviceNumber = effectiveDateRanges.deviceNumber 
         AND dimdevice._recordEnd >= effectiveDateRanges._effectiveFrom 
         AND dimdevice._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimdevice._recordDeleted = 0
 LEFT OUTER JOIN curated_v2.dimDeviceHistory dimdevicehistory
     ON dimdevicehistory.deviceNumber = effectiveDateRanges.deviceNumber 
         AND dimdevicehistory._recordEnd >= effectiveDateRanges._effectiveFrom 
         AND dimdevicehistory._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimdevicehistory._recordDeleted = 0
 LEFT OUTER JOIN curated_v2.dimDeviceInstallationHistory dimdeviceinstallationhistory
     ON dimdeviceinstallationhistory.logicalDeviceNumber = dimdevicehistory.logicalDeviceNumber
         AND dimdevicehistory._recordStart >= dimdeviceinstallationhistory._recordStart
         AND dimdevicehistory._recordStart <= dimdeviceinstallationhistory._recordEnd
+        AND dimdeviceinstallationhistory._recordDeleted = 0
 LEFT OUTER JOIN curated_v2.dimRegisterHistory dimregisterhistory
     ON dimregisterhistory.deviceNumber = dimdevice.deviceNumber
       AND dimDeviceHistory._recordStart >= dimregisterhistory._recordStart
       AND dimDeviceHistory._recordStart <= dimregisterhistory._recordEnd
+      AND dimregisterhistory._recordDeleted = 0
 LEFT OUTER JOIN curated_v2.dimRegisterInstallationHistory dimregisterinstallationhistory
     ON dimregisterinstallationhistory.installationNumber = dimdeviceinstallationhistory.installationNumber
       AND dimregisterhistory.logicalRegisterNumber = dimregisterinstallationhistory.logicalRegisterNumber
       AND dimDeviceHistory._recordStart >= dimregisterinstallationhistory._recordStart
       AND dimDeviceHistory._recordStart <= dimregisterhistory._recordEnd
+      AND dimregisterinstallationhistory._recordDeleted = 0
 LEFT OUTER JOIN cleansed.isu_0UCINSTALLA_ATTR_2 installAttr
     ON installAttr.installationNumber = dimdeviceinstallationhistory.installationNumber
-ORDER BY effectiveDateRanges._effectiveFrom
+    AND installAttr._recordDeleted = 0
+UNION
+SELECT
+     dimdevice.deviceSK
+    ,dimdevicehistory.deviceHistorySK
+    ,dimdevice.sourceSystemCode
+    ,coalesce(dimdevice.deviceNumber, dimdevicehistory.deviceNumber, -1) as deviceNumber
+    ,dimdevice.materialNumber
+    ,dimdevice.deviceID
+    ,dimdevice.inspectionRelevanceIndicator
+    ,dimdevice.deviceSize
+    ,dimdevice.assetManufacturerName
+    ,dimdevice.manufacturerSerialNumber
+    ,dimdevice.manufacturerModelNumber
+    ,dimdevice.objectNumber
+    ,dimdevice.functionClassCode
+    ,dimdevice.functionClass
+    ,dimdevice.constructionClassCode
+    ,dimdevice.constructionClass
+    ,dimdevice.deviceCategoryName
+    ,dimdevice.deviceCategoryDescription
+    ,dimdevice.ptiNumber
+    ,dimdevice.ggwaNumber
+    ,dimdevice.certificationRequirementType
+    ,dimdevicehistory.validToDate AS deviceHistoryValidToDate
+    ,dimdevicehistory.validFromDate AS deviceHistoryValidFromDate
+    ,dimdevicehistory.logicalDeviceNumber
+    ,dimdevicehistory.deviceLocation
+    ,dimdevicehistory.deviceCategoryCombination
+    ,dimdevicehistory.registerGroupCode
+    ,dimdevicehistory.registerGroup
+    ,dimdevicehistory.installationDate
+    ,dimdevicehistory.deviceRemovalDate
+    ,dimdevicehistory.activityReasonCode
+    ,dimdevicehistory.activityReason
+    ,dimdevicehistory.firstInstallationDate
+    ,dimdevicehistory.lastDeviceRemovalDate
+    ,dimdeviceinstallationhistory.deviceInstallationHistorySK
+    ,dimdeviceinstallationhistory.installationNumber
+    ,dimdeviceinstallationhistory.validToDate AS deviceInstallationHistoryValidToDate
+    ,dimdeviceinstallationhistory.validFromDate AS deviceInstallationHistoryValidFromDate
+    ,dimdeviceinstallationhistory.priceClassCode
+    ,dimdeviceinstallationhistory.priceClass
+    ,dimdeviceinstallationhistory.rateTypeCode AS deviceInstallationHistoryRateTypeCode
+    ,dimdeviceinstallationhistory.rateType AS deviceInstallationHistoryRateType
+    ,dimregisterhistory.registerHistorySK
+    ,dimregisterhistory.registerNumber
+    ,dimregisterhistory.validToDate AS registerHistoryValidToDate
+    ,dimregisterhistory.validFromDate AS registerHistoryvalidFromDate
+    ,dimregisterhistory.logicalRegisterNumber
+    ,dimregisterhistory.divisionCategoryCode
+    ,dimregisterhistory.divisionCategory
+    ,dimregisterhistory.registerIdCode
+    ,dimregisterhistory.registerId
+    ,dimregisterhistory.registerTypeCode
+    ,dimregisterhistory.registerType
+    ,dimregisterhistory.registerCategoryCode
+    ,dimregisterhistory.registerCategory
+    ,dimregisterhistory.reactiveApparentOrActiveRegisterCode
+    ,dimregisterhistory.reactiveApparentOrActiveRegister
+    ,dimregisterhistory.unitOfMeasurementMeterReading
+    ,dimregisterhistory.doNotReadFlag	
+    ,dimregisterinstallationhistory.registerInstallationHistorySK
+    ,dimregisterinstallationhistory.validToDate AS registerInstallationHistoryValidToDate
+    ,dimregisterinstallationhistory.validFromDate AS registerInstallationHistoryValidFromDate
+    ,dimregisterinstallationhistory.operationCode
+    ,dimregisterinstallationhistory.operationDescription
+    ,dimregisterinstallationhistory.rateTypeCode AS registerInstallationHistoryRateTypeCode
+    ,dimregisterinstallationhistory.rateType AS registerInstallationHistoryRateType
+    ,dimregisterinstallationhistory.registerNotRelevantToBilling
+    ,dimregisterinstallationhistory.rateFactGroupCode
+    ,installAttr.divisionCode
+    ,installAttr.division
+    ,effectiveDateRanges._effectiveFrom
+    ,effectiveDateRanges._effectiveTo
+    ,dimdevice._recordDeleted as _dimDeviceRecordDeleted
+    ,dimdevicehistory._recordDeleted as _dimDeviceHistoryRecordDeleted
+    ,dimdevice._recordCurrent as _dimDeviceRecordCurrent
+    ,dimdevicehistory._recordCurrent as _dimDeviceHistoryRecordCurrent
+FROM effectiveDaterangesDelete as effectiveDateRanges
+LEFT OUTER JOIN curated_v2.dimDevice dimdevice
+    ON dimdevice.deviceNumber = effectiveDateRanges.deviceNumber 
+        AND dimdevice._recordEnd >= effectiveDateRanges._effectiveFrom 
+        AND dimdevice._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimdevice._recordDeleted = 1
+LEFT OUTER JOIN curated_v2.dimDeviceHistory dimdevicehistory
+    ON dimdevicehistory.deviceNumber = effectiveDateRanges.deviceNumber 
+        AND dimdevicehistory._recordEnd >= effectiveDateRanges._effectiveFrom 
+        AND dimdevicehistory._recordStart <= effectiveDateRanges._effectiveTo
+        AND dimdevicehistory._recordDeleted = 1
+LEFT OUTER JOIN curated_v2.dimDeviceInstallationHistory dimdeviceinstallationhistory
+    ON dimdeviceinstallationhistory.logicalDeviceNumber = dimdevicehistory.logicalDeviceNumber
+        AND dimdevicehistory._recordStart >= dimdeviceinstallationhistory._recordStart
+        AND dimdevicehistory._recordStart <= dimdeviceinstallationhistory._recordEnd
+        AND dimdeviceinstallationhistory._recordDeleted = 1
+LEFT OUTER JOIN curated_v2.dimRegisterHistory dimregisterhistory
+    ON dimregisterhistory.deviceNumber = dimdevice.deviceNumber
+      AND dimDeviceHistory._recordStart >= dimregisterhistory._recordStart
+      AND dimDeviceHistory._recordStart <= dimregisterhistory._recordEnd
+      AND dimregisterhistory._recordDeleted = 1
+LEFT OUTER JOIN curated_v2.dimRegisterInstallationHistory dimregisterinstallationhistory
+    ON dimregisterinstallationhistory.installationNumber = dimdeviceinstallationhistory.installationNumber
+      AND dimregisterhistory.logicalRegisterNumber = dimregisterinstallationhistory.logicalRegisterNumber
+      AND dimDeviceHistory._recordStart >= dimregisterinstallationhistory._recordStart
+      AND dimDeviceHistory._recordStart <= dimregisterhistory._recordEnd
+      AND dimregisterinstallationhistory._recordDeleted = 1
+LEFT OUTER JOIN cleansed.isu_0UCINSTALLA_ATTR_2 installAttr
+    ON installAttr.installationNumber = dimdeviceinstallationhistory.installationNumber
+    AND installAttr._recordDeleted = 1
+)
+ORDER BY _effectiveFrom
 
 -- COMMAND ----------
 
