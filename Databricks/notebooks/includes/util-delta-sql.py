bc0012635e2879d74bc4886d767fa7309b8a4537
opd4@sydneywater.com.au <opd4@sydneywater.com.au>
2022-11-23 04:52:19 +0000

    reject functionality

--- Databricks/notebooks/includes/util-delta-sql.py
+++ Databricks/notebooks/includes/util-delta-sql.py
@@ -669,8 +669,7 @@ from pyspark.sql.functions import DataFrame
 
 def DeltaSaveDataFrameToRejectTable(dataframe,target_table,business_key,source_key,lastExecutionTS):
   #This method uses the dataframe to load data into Cleansed Rejected Table
-    reject_table = 'rejected.cleansed_rejected'
-    cleansed_table = f"rejected.{target_table}"
+    reject_table = 'cleansed_rejected'
     raw_table = f"raw.{target_table}"
     
     #Build cleansed reject dataframe
@@ -692,12 +691,14 @@ def DeltaSaveDataFrameToRejectTable(dataframe,target_table,business_key,source_k
     display(reject_df)
 
     #Save the reject dataframe to generic reject table (cleansed_reject)    
-    data_lake_path = DeltaGetDataLakePath(ADS_DATALAKE_ZONE_REJECTED, ADS_DATABASE_REJECTED, reject_table)  
+    data_lake_path = DeltaGetDataLakePath(ADS_DATALAKE_ZONE_REJECTED, ADS_DATABASE_REJECTED, reject_table) 
+    print(data_lake_path)
     LogEtl(f"write to reject table")
-    reject_df.write.mode("append").option("overwriteSchema","true").option("path", data_lake_path).saveAsTable(reject_table) 
+    reject_df.write.mode("append").option("overwriteSchema","true").option("path", data_lake_path).saveAsTable(f"rejected.{reject_table}") 
     
     #Save the reject dataframe to individual reject table
-    data_lake_path = DeltaGetDataLakePath(ADS_DATALAKE_ZONE_REJECTED, ADS_DATABASE_REJECTED, cleansed_table)
+    data_lake_path = DeltaGetDataLakePath(ADS_DATALAKE_ZONE_REJECTED, ADS_DATABASE_REJECTED, target_table)
+    print(data_lake_path)
     #Drop columns 
     cleansed_df = dataframe.drop("sourceKeyDesc","sourceKey","rejectColumn")
-    dataframe.write.mode("append").option("overwriteSchema","true").option("path", data_lake_path).saveAsTable(cleansed_table) 
+    dataframe.write.mode("append").option("overwriteSchema","true").option("path", data_lake_path).saveAsTable(f"rejected.{target_table}") 
