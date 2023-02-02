368231e7725d8ee6972c0c814a3206376ca27b1c
PRAGATHI SHARMA <o7p3@sydneywater.com.au>
2023-02-02 02:32:29 +0000

    Merged PR 2927: labware cleansed mapping changes

---
+++ Databricks/Automated-Testing-Framework/tests/cleansed/labwareref.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %run ../../common/common-atf
+
+# COMMAND ----------
+
+#set system code to notebook name
+list = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get().split("/")
+systemCode = list[len(list)-1]
+#get full list of tables to be tested from extract load manifest where the system code matches and the extract is enabled
+df = spark.table("controldb.dbo_extractLoadManifest").where(f"systemCode = '{systemCode}' and enabled = 1")
+
+# COMMAND ----------
+
+#define mapping document path and sheet name
+DOC_PATH = 'dbfs:/mnt/data/mapping_documents_UC3/Labware_Raw2Cleansed_v1.xlsx'
+SHEET_NAME = 'cleansed_mapping'
+
+# COMMAND ----------
+
+#loop through df and run tests for each table printing out results
+for i in df.rdd.collect():
+    TABLE_FQN = f"cleansed.{i.DestinationSchema}_{i.DestinationTableName}"
+    print(f"Running tests for table: {TABLE_FQN}...")
+    RunTests()
+
+# COMMAND ----------
+
+
