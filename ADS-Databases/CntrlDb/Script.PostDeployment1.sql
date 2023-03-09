fda5dc9433f7cd23cc080b06329ecbbd528cb35f
ANTONIO IPILI <o63p@sydneywater.com.au>
2023-03-09 02:06:50 +0000

    Merged PR 3249: Added support to overrider common config
    Added support to overrider common config
    
    Cherry picked from !3248

--- ADS-Databases/CntrlDb/Script.PostDeployment1.sql
+++ ADS-Databases/CntrlDb/Script.PostDeployment1.sql
@@ -1,8 +1,7 @@
 ﻿DECLARE @SummaryOfChanges TABLE(Change VARCHAR(20));
-MERGE INTO [dbo].[Config] AS TGT
-USING (
+WITH [_Common] AS
+(
     SELECT *
-    ,CONVERT(DATETIME, CONVERT(DATETIMEOFFSET, GETDATE()) AT TIME ZONE 'AUS Eastern Standard Time') [CreatedDTS]
     FROM (
         SELECT 'IngestionDefault' [KeyGroup], 'RawPath' [Key], '/raw/$SYSTEM$/$SCHEMA$_$TABLE$/$yyyy$/$MM$/$dd$/$HH$$mm$/$guid$.$EXT$' [Value]
         UNION SELECT 'IngestionDefault' [KeyGroup], 'CleansedPath' [Key], '/cleansed/$SYSTEM$/$SCHEMA$_$TABLE$/' [Value]
@@ -11,10 +10,34 @@ USING (
         UNION SELECT 'IngestionSheet' [KeyGroup], 'Index' [Key], '0' [Value]
         UNION SELECT 'IngestionSheet' [KeyGroup], 'Limit' [Key], '0' [Value]
         UNION SELECT 'IngestionSheet' [KeyGroup], 'Path' [Key], 'testonly/labware_upload.csv' [Value]
-        UNION SELECT 'TriggerInterval' [KeyGroup], '15Min' [Key], 'maximo|15min' [Value]
+        UNION SELECT 'TriggerInterval' [KeyGroup], '15Min' [Key], 'maximo|15min,iicats|15min' [Value]
     ) T
+),
+[_Env] AS
+(
+    SELECT Value from dbo.config where [KeyGroup] = 'System' and [Key] = 'Environment' 
+),
+[_Override] AS
+(
+    SELECT *
+    FROM (
+        SELECT 'cleansedLayer' [KeyGroup], 'skipIngestion' [Key], 'iicats,scada' [Value]
+    ) O
+    WHERE EXISTS(SELECT * FROM [_Env] WHERE VALUE = 'PROD')
+),
+[_SourceConfig] AS
+(
+    SELECT  COALESCE([_Override].[KeyGroup], [_Common].[KeyGroup]) [KeyGroup]
+        ,COALESCE([_Override].[Key], [_Common].[Key]) [Key]
+        ,COALESCE([_Override].[Value], [_Common].[Value]) [Value]
+        ,CONVERT(DATETIME, CONVERT(DATETIMEOFFSET, GETDATE()) AT TIME ZONE 'AUS Eastern Standard Time') [CreatedDTS]
+    FROM [_Common]
+    FULL JOIN [_Override]
+    ON ([_Common].[KeyGroup] = [_Override].[KeyGroup] and [_Common].[Key] = [_Override].[Key])
 )
-AS SRC ([KeyGroup],[Key],[Value],[CreatedDTS]) ON TGT.[KeyGroup] = SRC.[KeyGroup] AND TGT.[Key] = SRC.[Key]
+MERGE INTO [dbo].[Config] AS TGT
+USING [_SourceConfig]
+AS SRC ON TGT.[KeyGroup] = SRC.[KeyGroup] AND TGT.[Key] = SRC.[Key]
 WHEN NOT MATCHED BY TARGET THEN
     INSERT ([KeyGroup],[Key],[Value],[CreatedDTS]) VALUES ([KeyGroup],[Key],[Value],[CreatedDTS])
 OUTPUT $action 
