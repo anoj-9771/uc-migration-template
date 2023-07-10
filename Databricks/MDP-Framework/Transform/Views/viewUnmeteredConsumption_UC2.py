3e5c2d64dfecc439fd9536e0e374592dc24cd6f8
PAVAN PARAMESWAR <ov8z@sydneywater.com.au>
2023-07-10 00:44:48 +0000

    Merged PR 4711: view fixes and aggregate metrics

--- Databricks/MDP-Framework/Transform/Views/viewUnmeteredConsumption_UC2.py
+++ Databricks/MDP-Framework/Transform/Views/viewUnmeteredConsumption_UC2.py
@@ -18,16 +18,14 @@ CREATE OR REPLACE VIEW {get_table_namespace(f'{DEFAULT_TARGET}', 'viewUnmeteredC
   propertyCount,
   reportDate)
 AS select 'Unmetered Connected' as unmeteredConsumptionType, dw.superiorPropertyTypeCode, dw.superiorPropertyType, dw.inferiorPropertyTypeCode, dw.inferiorPropertyType, sum(fc.consumptionQuantity) as consumptionQuantity, count(fc.propertyNumber) as propertyCount, fc.reportDate
-from {get_table_namespace(f'{DEFAULT_TARGET}', 'factunmeteredconsumption')} fc, {get_table_namespace(f'{DEFAULT_TARGET}', 'dimpropertytypehistory')} dw
-where fc.propertyTypeHistorySK = dw.propertyTypeHistorySK
-and fc.unmeteredConnectedFlag ='Y'
+from {get_table_namespace(f'{DEFAULT_TARGET}', 'factunmeteredconsumption')} fc left outer join {get_table_namespace(f'{DEFAULT_TARGET}', 'dimpropertytypehistory')} dw on fc.propertyTypeHistorySK = dw.propertyTypeHistorySK
+where fc.unmeteredConnectedFlag ='Y'
 and fc.reportDate = (select max(reportDate) from {get_table_namespace(f'{DEFAULT_TARGET}', 'factunmeteredconsumption')} where unmeteredConnectedFlag ='Y')
 group by fc.unmeteredConnectedFlag, dw.superiorPropertyTypeCode, dw.superiorPropertyType, dw.inferiorPropertyTypeCode, dw.inferiorPropertyType, fc.reportDate
 union all
 select 'Unmetered Construction' as unmeteredConsumptionType, dw.superiorPropertyTypeCode, dw.superiorPropertyType, dw.inferiorPropertyTypeCode, dw.inferiorPropertyType, sum(fc.consumptionQuantity) as consumptionQuantity, count(fc.propertyNumber) as propertyCount, fc.reportDate
-from {get_table_namespace(f'{DEFAULT_TARGET}', 'factunmeteredconsumption')} fc, {get_table_namespace(f'{DEFAULT_TARGET}', 'dimpropertytypehistory')} dw
-where fc.propertyTypeHistorySK = dw.propertyTypeHistorySK
-and fc.unmeteredconstructionflag ='Y'
+from {get_table_namespace(f'{DEFAULT_TARGET}', 'factunmeteredconsumption')} fc left outer join {get_table_namespace(f'{DEFAULT_TARGET}', 'dimpropertytypehistory')} dw on fc.propertyTypeHistorySK = dw.propertyTypeHistorySK
+where fc.unmeteredconstructionflag ='Y'
 and fc.reportDate = (select max(reportDate) from {get_table_namespace(f'{DEFAULT_TARGET}', 'factunmeteredconsumption')} where unmeteredconstructionflag ='Y')
 group by fc.unmeteredconstructionflag, dw.superiorPropertyTypeCode, dw.superiorPropertyType, dw.inferiorPropertyTypeCode, dw.inferiorPropertyType, fc.reportDate
 order by 1 asc, dw.superiorPropertyTypeCode, dw.superiorPropertyType, dw.inferiorPropertyTypeCode, dw.inferiorPropertyType, fc.reportDate
@@ -47,15 +45,13 @@ CREATE OR REPLACE VIEW {get_table_namespace(f'{DEFAULT_TARGET}', 'viewUnmeteredC
   consumptionQuantity,
   reportDate)
 AS select 'Unmetered Connected' as unmeteredConsumptionType, dw.deliverySystem, dw.distributionSystem, dw.supplyZone, dw.pressureArea, fc.propertyCount, fc.consumptionQuantity, fc.reportDate
-from {get_table_namespace(f'{DEFAULT_TARGET}', 'factconsumptionaggregate')} fc, {get_table_namespace(f'{DEFAULT_TARGET}', 'dimwaterNetwork')} dw
-where fc.waterNetworkSK = dw.waterNetworkSK
-and fc.unmeteredConnectedFlag ='Y'
+from {get_table_namespace(f'{DEFAULT_TARGET}', 'factconsumptionaggregate')} fc left outer join {get_table_namespace(f'{DEFAULT_TARGET}', 'dimwaterNetwork')} dw on fc.waterNetworkSK = dw.waterNetworkSK
+where fc.unmeteredConnectedFlag ='Y'
 and fc.reportDate = (select max(reportDate) from {get_table_namespace(f'{DEFAULT_TARGET}', 'factconsumptionaggregate')} where unmeteredConnectedFlag ='Y')
 union all
 select 'Unmetered Construction' as unmeteredConsumptionType, dw.deliverySystem, dw.distributionSystem, dw.supplyZone, dw.pressureArea, fc.propertyCount, fc.consumptionQuantity, fc.reportDate
-from {get_table_namespace(f'{DEFAULT_TARGET}', 'factconsumptionaggregate')} fc, {get_table_namespace(f'{DEFAULT_TARGET}', 'dimwaterNetwork')} dw
-where fc.waterNetworkSK = dw.waterNetworkSK
-and fc.unmeteredConstructionFlag ='Y'
+from {get_table_namespace(f'{DEFAULT_TARGET}', 'factconsumptionaggregate')} fc left outer join {get_table_namespace(f'{DEFAULT_TARGET}', 'dimwaterNetwork')} dw on fc.waterNetworkSK = dw.waterNetworkSK
+where fc.unmeteredConstructionFlag ='Y'
 and fc.reportDate = (select max(reportDate) from {get_table_namespace(f'{DEFAULT_TARGET}', 'factconsumptionaggregate')} where unmeteredConstructionFlag ='Y')
 order by 1 asc, dw.deliverySystem, dw.distributionSystem, dw.supplyZone, dw.pressureArea, fc.reportDate
 """)
@@ -67,9 +63,10 @@ spark.sql(f"""
 CREATE OR REPLACE VIEW {get_table_namespace(f'{DEFAULT_TARGET}', 'viewUnmeteredConsumptionSystemLevel')} (
   unmeteredConsumptionType,
   consumptionQuantity,
+  publicReservesAllocation,
   propertyCount,
   reportDate)
-AS select unmeteredConsumptionType, case when unmeteredConsumptionType = 'Unmetered Connected' then sum(consumptionQuantity)+1000 else sum(consumptionQuantity) end as consumptionQuantity, sum(propertyCount) as propertyCount, reportDate
+AS select unmeteredConsumptionType, sum(consumptionQuantity), case when unmeteredConsumptionType = 'Unmetered Connected' then 1000 else NULL end as publicReservesAllocation, sum(propertyCount) as propertyCount, reportDate
 from {get_table_namespace(f'{DEFAULT_TARGET}', 'viewUnmeteredConsumptionWaterNetwork')}
 group by UnmeteredConsumptionType, reportDate
 """)
