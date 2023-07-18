6470fffa65f1f5e9b701e4d9eacd52c621652384
NIDHIN ASOKAN <oj4m@sydneywater.com.au>
2023-07-18 02:16:02 +0000

    Merged PR 4820: p6_test_changes
    Cherry picked from !4455

---
+++ Databricks/MDP-Framework/ControlDB/ExtractLoadManifest/primavera-ingestion.py
@@ -0,0 +1,57 @@
+# Databricks notebook source
+# MAGIC %run /MDP-Framework/ControlDB/common-controldb
+
+# COMMAND ----------
+
+SYSTEM_CODE = "primavera"
+
+# COMMAND ----------
+
+df = spark.sql("""
+    WITH _Base AS 
+    (
+      SELECT 'primavera' SystemCode, 'primavera' SourceSchema, '' SourceKeyVaultSecret, 'nas-binary-load' SourceHandler, 'csv' RawFileExtension, 'raw-load-delta' RawHandler, '' ExtendedProperties, 'cleansed-load-delta' CleansedHandler, '' WatermarkColumn
+    )
+SELECT 'activeprojects'  SourceTableName ,'PRI_Activeprojects' as SourceQuery  ,* FROM _Base  
+    """)
+
+
+
+# COMMAND ----------
+
+def ConfigureManifest(df):
+    # ------------- CONSTRUCT QUERY ----------------- #
+    
+    # ------------- DISPLAY ----------------- #
+    ShowQuery(df)
+
+    # ------------- SAVE ----------------- #
+    AddIngestion(df)
+    
+    # ------------- ShowConfig ----------------- #
+    ShowConfig()
+    
+
+ConfigureManifest(df)       
+
+# COMMAND ----------
+
+#ADD BUSINESS KEY
+ExecuteStatement("""
+update dbo.extractLoadManifest set
+businessKeyColumn = case sourceTableName
+when 'activeprojects' then 'projectId,milestoneName,projectFinishdate,loadPeriod'
+else businessKeyColumn
+end
+where systemCode in ('primavera')
+""")
+
+# COMMAND ----------
+
+ExecuteStatement(f"update dbo.extractLoadManifest set enabled = 1 where systemCode = '{SYSTEM_CODE}'")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC select * from controldb.dbo_extractloadmanifest where systemCode = 'primavera'
