9baca2a20833f9e60df27ccc1538add313034bb6
KELVIN HONG <o3bj@sydneywater.com.au>
2023-08-29 02:52:10 +0000

    Merged PR 5141: Directing 15min loads to interactive cluster

--- ADS-Databases/CntrlDb/dbo/Procedures/ExtractLoadInit.sql
+++ ADS-Databases/CntrlDb/dbo/Procedures/ExtractLoadInit.sql
@@ -87,7 +87,7 @@ BEGIN
     ,[DestinationSchema]
     ,[DestinationTableName]
     ,[DestinationKeyVaultSecret]
-    ,[ExtendedProperties]
+    ,JSON_MODIFY(ISNULL([ExtendedProperties], '{}'),'$.OverrideClusterName', COALESCE(JSON_VALUE([ExtendedProperties], '$.OverrideClusterName') ,C5.[Value])) [ExtendedProperties]
 	,COALESCE(C2.Value, C1.Value) AS [QueryFilter]
 	,@ExtraConfig AS [ExtraConfig]
     ,COALESCE(C3.[Value], C4.[Value], 0) [WorkspaceSwitch]
@@ -97,6 +97,7 @@ BEGIN
 	LEFT JOIN [dbo].[Config] C2 ON C2.[KeyGroup] = R.[SystemCode] AND C2.[Key] = R.[SourceTableName]
     LEFT JOIN [dbo].[Config] C3 ON C3.[KeyGroup] = 'WorkspaceSwitch' AND C3.[Key] = R.[SystemCode]
 	LEFT JOIN [dbo].[Config] C4 ON C4.[KeyGroup] = 'WorkspaceSwitch' AND C4.[Key] = '_DefaultValue_'
+	LEFT JOIN [dbo].[Config] C5 ON C5.[KeyGroup] = 'OverrideClusterNameDefault' AND C5.[Key] = R.[SystemCode]
 	WHERE 
 	S.BatchID = @BatchID
 	AND S.SystemCode = @SystemCode
