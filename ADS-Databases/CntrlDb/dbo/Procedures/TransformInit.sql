39adbb66f8121c212604a61882d9782d3cf98e9c
KELVIN HONG <o3bj@sydneywater.com.au>
2023-09-01 02:41:54 +0000

    Merged PR 5192: Amend TransformInit OverrideClusterName ExtendedProperties

--- ADS-Databases/CntrlDb/dbo/Procedures/TransformInit.sql
+++ ADS-Databases/CntrlDb/dbo/Procedures/TransformInit.sql
@@ -27,12 +27,27 @@ BEGIN
 	SELECT 
 	S.BatchID
 	,S.ID
-	,M.*
+	,S.[TransformID]
+    ,[EntityType]
+    ,[EntityName]
+    ,[ProcessorType]
+    ,[TargetKeyVaultSecret]
+    ,[Command]
+    ,[Dependancies]
+    ,[ParallelGroup]
+    ,IIF(C4.[Value] = 1, 
+		JSON_MODIFY(
+			ISNULL([ExtendedProperties], '{}'), '$.OverrideClusterName', COALESCE(JSON_VALUE([ExtendedProperties], '$.OverrideClusterName'), C8.[Value], C7.[Value])
+		)
+	,[ExtendedProperties]) [ExtendedProperties]
+    ,[Enabled]
 	,(SELECT STRING_AGG([ParallelGroup], ',') [List] FROM (SELECT DISTINCT TOP 100 PERCENT [ParallelGroup] FROM [dbo].[TransformManifest] ORDER BY [ParallelGroup]) T) [List]
 	,COALESCE(C4.[Value], 0) [WorkspaceSwitch]
 	FROM [dbo].[TransformStatus] S
 	JOIN [dbo].[TransformManifest] M ON M.[TransformID] = S.[TransformID]
-	LEFT JOIN [dbo].[Config] C4 ON C4.[KeyGroup] = 'WorkspaceSwitch' AND C4.[Key] = '_DefaultValue_'
+	LEFT JOIN [dbo].[Config] C4 ON C4.[KeyGroup] = 'WorkspaceSwitch' AND C4.[Key] = '_Default_'
+	LEFT JOIN [dbo].[Config] C7 ON C7.[KeyGroup] = 'TransformOverrideClusterName' AND C7.[Key] = '_Default_'
+	LEFT JOIN [dbo].[Config] C8 ON C8.[KeyGroup] = 'TransformOverrideClusterName' AND C8.[Key] = [EntityName]
 	WHERE 
 	S.BatchID = @BatchID
 	AND M.[Enabled] = 1
